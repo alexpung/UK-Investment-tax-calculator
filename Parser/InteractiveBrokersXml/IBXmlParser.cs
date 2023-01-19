85f93c2a4ff7820196ecef472822582915954a68
Alex Pun <alexpung@protonmail.com>
2023-01-19 21:17:20 +0000

    Implement IB xml dividend parsing

---
+++ Parser/InteractiveBrokersXml/IBXmlParser.cs
@@ -0,0 +1,27 @@
+﻿using CapitalGainCalculator.Enum;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CapitalGainCalculator.Parser.InteractiveBrokersXml
+{
+    public class IBXmlParser
+    {
+        public IEnumerable<XElement> ParseDividend(XElement document)
+        {
+            return document.Descendants("CashTransaction").Where(row => GetDividendType(row.Attribute("type")?.Value) != DividendType.NOT_DIVIDEND);
+        }
+
+        private DividendType GetDividendType(string? dividendTypeString) => dividendTypeString switch
+        {
+            "Withholding Tax" => DividendType.WITHHOLDING,
+            "Dividends" => DividendType.DIVIDEND,
+            "Payment In Lieu Of Dividends" => DividendType.DIVIDEND_IN_LIEU,
+            _ => DividendType.NOT_DIVIDEND
+        };
+    }
+}
---
+++ Test/IBXmlParseTest.cs
@@ -0,0 +1,48 @@
+﻿using CapitalGainCalculator.Enum;
+using CapitalGainCalculator.Parser.InteractiveBrokersXml;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CapitalGainCalculator.Test
+{
+    public class IBXmlParseTest
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        public IBXmlParseTest(ITestOutputHelper output)
+        {
+            _testOutputHelper = output;
+        }
+        [Fact]
+        public void TestReadingIBXmlDividends()
+        {
+            XElement xmlDoc = XElement.Load(@".\Test\Resource\TaxExample.xml");
+            IBXmlParser iBXmlParser = new IBXmlParser();
+            IEnumerable<XElement> parsedData = iBXmlParser.ParseDividend(xmlDoc);
+            parsedData.Count().ShouldBe(93);
+            IEnumerable<XElement> witholdingTaxes = parsedData.Where(row => row.Attribute("type")?.Value == "Withholding Tax");
+            IEnumerable<XElement> dividends = parsedData.Where(row => row.Attribute("type")?.Value == "Dividends");
+            IEnumerable<XElement> dividendsInLieu = parsedData.Where(row => row.Attribute("type")?.Value == "Payment In Lieu Of Dividends");
+            witholdingTaxes.Sum(row => decimal.Parse(row.Attribute("amount")?.Value ?? "0")).ToString().ShouldBe("-363394");
+            dividends.Sum(row => decimal.Parse(row.Attribute("amount")?.Value ?? "0")).ToString().ShouldBe("2367244.68");
+            dividendsInLieu.Sum(row => decimal.Parse(row.Attribute("amount")?.Value ?? "0")).ToString().ShouldBe("933.84");
+            PrintXElements(dividends);
+        }
+
+        private void PrintXElements(IEnumerable<XElement> results)
+        {
+            foreach (XElement element in results)
+            {
+                _testOutputHelper.WriteLine(element.ToString());
+            }
+        }
+    }
+}
