02adc7dc6b68b17b90b71d083eac89cd2003c262
Alex Pun <alexpung@protonmail.com>
2023-01-26 21:41:11 +0000

    Initial draft of the tax event model

---
+++ Model/TaxEvent.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NodaMoney;
+
+namespace CapitalGainCalculator.Model
+{
+    public abstract record TaxEvent
+    {
+        public string AssetName { get; }
+        public DateTime Date { get; }
+
+        protected TaxEvent(string assetName, DateTime date)
+        {
+           AssetName = assetName;
+           Date = date;
+        }
+    }
+
+    public record BuyTrade : TaxEvent
+    {
+        public decimal Quantity { get; }
+        public DescribedMoney Proceed { get; }
+        public IEnumerable<DescribedMoney>? Expenses { get; }
+        public BuyTrade(string assetName, DateTime date, decimal quantity, DescribedMoney proceed, IEnumerable<DescribedMoney>? expenses = null) : base(assetName, date)
+        {
+            Quantity = quantity;
+            Proceed = proceed;
+            Expenses = expenses;
+        }
+    }
+
+    public record SellTrade : TaxEvent
+    {
+        public decimal Quantity { get; }
+        public DescribedMoney Proceed { get; }
+        public IEnumerable<DescribedMoney>? Expenses { get; }
+        public SellTrade(string assetName, DateTime date, decimal quantity, DescribedMoney proceed, IEnumerable<DescribedMoney>? expenses = null) : base(assetName, date)
+        {
+            Quantity = quantity;
+            Proceed = proceed;
+            Expenses = expenses;
+        }
+    }
+
+    public record Dividend : TaxEvent
+    {
+        public RegionInfo CompanyLocation { get; }
+        public DescribedMoney Proceed { get; }
+        public ExchangeRate ExchangeRate { get; }
+        public IEnumerable<DescribedMoney>? Expenses { get; }
+        public Dividend(string assetName, DateTime date, RegionInfo companyLocation, DescribedMoney proceeds, ExchangeRate exchangeRate, IEnumerable<DescribedMoney>? expenses = null) : base(assetName, date)
+        {
+            Proceed = proceeds;
+            Expenses = expenses;
+            CompanyLocation = companyLocation;
+            ExchangeRate = exchangeRate;
+        }
+    }
+
+    public record StockSplit : TaxEvent
+    {
+        public ushort NumberBeforeSplit { get; }
+        public ushort NumberAfterSplit { get; }
+        public StockSplit(string assetName, DateTime date, ushort numberBeforeSplit, ushort numberAfterSplit) : base(assetName, date)
+        {
+            NumberBeforeSplit = numberBeforeSplit;
+            NumberAfterSplit = numberAfterSplit;
+        }
+    }
+
+    public record DescribedMoney
+    {
+        public string? Description { get; }
+        public Money Amount { get; }
+
+        public ExchangeRate ExchangeRate { get; }
+        public DescribedMoney(Money amount, ExchangeRate exchangeRate, string? description = null)
+        {
+            Description = description;
+            Amount = amount;
+            ExchangeRate = exchangeRate;
+        }
+    }
+
+}
