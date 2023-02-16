335a4634b5de21a9c78a4dff69d118b5f7857bed
Alex Pun <alexpung@protonmail.com>
2023-02-16 21:25:08 +0000

    Add TradeTaxCalculation class and group trade according to UK tax rule

---
+++ Model/TradeTaxCalculation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalGainCalculator.Model
+{
+    public class TradeTaxCalculation
+    {
+        private readonly List<Trade> _tradeList;
+        public decimal TotaNetlAmount { get; }
+        private decimal _unmatchedNetAmount;
+        public decimal UnmatchedNetAmount
+        {
+            get { return _unmatchedNetAmount; }
+            set
+            {
+                _unmatchedNetAmount = value;
+                if(UnmatchedNetAmount == 0) CalculationCompleted = true;
+            }
+        }
+        public decimal TotalQty { get; }
+        public decimal UnmatchedQty { get; set; }
+
+        public bool CalculationCompleted { get; private set; }
+
+        public TradeTaxCalculation(IEnumerable<Trade> trades)
+        {
+            _tradeList = trades.ToList();
+            TotaNetlAmount = trades.Sum(CalculateNetAmount);
+            UnmatchedNetAmount = TotaNetlAmount;
+            TotalQty = trades.Sum(trade => trade.Quantity);
+            UnmatchedQty = TotalQty;
+            CalculationCompleted = false;
+        }
+
+        private decimal CalculateNetAmount(Trade trade)
+        {
+            decimal deductiable;
+            if (trade.Expenses.Any())
+            {
+                deductiable = trade.Expenses.Sum(expense => expense.BaseCurrencyAmount);
+            }
+            else deductiable = 0;
+            return trade.Proceed.BaseCurrencyAmount - deductiable;
+        }
+    }
+}
