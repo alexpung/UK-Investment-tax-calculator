47e368223e245e70144430a9cbee3971f0004502
Alex Pun <alexpung@protonmail.com>
2023-03-04 13:01:51 +0000

    Implement trade matching record to show calculation steps

---
+++ Model/TradeMatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalGainCalculator.Model
+{
+    /// <summary>
+    /// Data class to provide sufficient information to describe a matching of a trade pair and calculate taxable gain/loss
+    /// </summary>
+    public record TradeMatch
+    {
+        public System.Enum? TradeMatchType { get; set; }
+        public List<TradeTaxCalculation> MatchedGroups { get; set; } = new List<TradeTaxCalculation>();
+
+        public decimal MatchQuantity { get; set; } = 0m;
+
+        public decimal BaseCurrencyMatchValue { get; set; } = 0m;
+
+    }
+}
---
+++ Model/UkTaxModel/UkMatchType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalGainCalculator.Model.UkTaxModel
+{
+    public enum UkMatchType
+    {
+        SAME_DAY,
+        BREAD_AND_BREAKFAST,
+        SECTION_104
+    }
+}
