1c15ef5271ea4fe9b3d4294c420e97b474d2618d
Alex Pun <alexpung@protonmail.com>
2023-04-13 23:09:03 +0100

    Add interface to Calculator class

---
+++ Model/Interfaces/ICalculator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CapitalGainCalculator.Model.Interfaces;
+
+public interface ICalculator
+{
+    public void AddTaxEvents(TaxEventLists taxEventLists);
+    public List<TradeTaxCalculation> CalculateTax();
+}
