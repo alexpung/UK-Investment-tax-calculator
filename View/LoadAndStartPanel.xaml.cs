099a990f0ec14d92e834334e7ca5d994950d0aa9
Alex Pun <alexpung@protonmail.com>
2023-04-13 23:10:04 +0100

    Implement LoadAndStart controls

--- View/LoadAndStartPanel.xaml.cs
+++ View/LoadAndStartPanel.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Autofac;
+using CapitalGainCalculator.ViewModel;
+using System.ComponentModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CapitalGainCalculator.View;
 /// <summary>
@@ -22,5 +12,9 @@ public partial class LoadAndStartPanel : UserControl
     public LoadAndStartPanel()
     {
         InitializeComponent();
+        if (!DesignerProperties.GetIsInDesignMode(this))
+        {
+            DataContext = App.Current.IocContainer.Resolve<LoadAndStartViewModel>();
+        }
     }
 }
