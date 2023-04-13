3a76239e2b4a449233c2f7c52d0dfb7ae660ba4d
Alex Pun <alexpung@protonmail.com>
2023-04-13 23:09:29 +0100

    Add LoadDataPage

---
+++ View/Page/LoadDataPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Wpf.Ui.Controls;
+
+namespace CapitalGainCalculator.View.Page;
+/// <summary>
+/// Interaction logic for LoadDataPage.xaml
+/// </summary>
+public partial class LoadDataPage : UiPage
+{
+    public LoadDataPage()
+    {
+        InitializeComponent();
+    }
+}
