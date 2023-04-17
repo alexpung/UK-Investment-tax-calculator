3f44971c204b9ee82032f1281bf12cfcb9882194
Alex Pun <alexpung@protonmail.com>
2023-04-17 23:40:12 +0100

    fix incorrect initial AssetTypeToLoadSetting

--- Model/AssetTypeToLoadSetting.cs
+++ Model/AssetTypeToLoadSetting.cs
@@ -2,8 +2,8 @@
 public class AssetTypeToLoadSetting
 {
     public bool LoadStocks { get; set; } = true;
-    public bool LoadOptions { get; set; } = true;
+    public bool LoadOptions { get; set; }
     public bool LoadFutures { get; set; }
     public bool LoadFx { get; set; }
-    public bool LoadDividends { get; set; }
+    public bool LoadDividends { get; set; } = true;
 }
