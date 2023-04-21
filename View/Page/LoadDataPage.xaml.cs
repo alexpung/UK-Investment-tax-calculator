dfd2c1a4ab0d2919bf9e5d5da1d2193302753a2d
Alex Pun <alexpung@protonmail.com>
2023-04-21 22:51:08 +0100

    Add UI to export Dividends

--- View/Page/LoadDataPage.xaml.cs
+++ View/Page/LoadDataPage.xaml.cs
@@ -6,11 +6,12 @@ namespace CapitalGainCalculator.View.Page;
 /// </summary>
 public partial class LoadDataPage : UiPage
 {
-    public LoadDataPage(LoadAndStartPanel loadAndStartPanel, LoadedFilesStatisticsPanel loadedFilesStatisticsPanel, CalculationSummaryPanel calculationSummaryPanel)
+    public LoadDataPage(LoadAndStartPanel loadAndStartPanel, LoadedFilesStatisticsPanel loadedFilesStatisticsPanel, CalculationSummaryPanel calculationSummaryPanel, ExportToFilePanel exportToFilePanel)
     {
         InitializeComponent();
         LoadDataPageMainStackPanel.Children.Add(loadAndStartPanel);
         LoadDataPageMainStackPanel.Children.Add(loadedFilesStatisticsPanel);
         LoadDataPageMainStackPanel.Children.Add(calculationSummaryPanel);
+        LoadDataPageMainStackPanel.Children.Add(exportToFilePanel);
     }
 }
---
+++ ViewModel/ExportToFileViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CapitalGainCalculator.Model;
+using CapitalGainCalculator.Services;
+using CapitalGainCalculator.ViewModel.Options;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CapitalGainCalculator.ViewModel;
+
+public partial class ExportToFileViewModel : ObservableObject
+{
+    private readonly DividendExportService _dividendExportService;
+    private readonly DividendCalculationResult _dividendCalculationResult;
+    private readonly YearOptions _yearOptions;
+
+    public ExportToFileViewModel(DividendExportService dividendExportService, DividendCalculationResult dividendCalculationResult, YearOptions years)
+    {
+        _dividendExportService = dividendExportService;
+        _dividendCalculationResult = dividendCalculationResult;
+        _yearOptions = years;
+    }
+
+    [RelayCommand]
+    public void ExportDividendToFile()
+    {
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.FileName = "Dividend Summary.txt";
+        saveFileDialog.DefaultExt = ".txt";
+        saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+        bool? result = saveFileDialog.ShowDialog();
+        if (result == true)
+        {
+            string filename = saveFileDialog.FileName;
+            IEnumerable<DividendSummary> filteredDividendSummary = _dividendCalculationResult.DividendSummary.Where(dividend => _yearOptions.IsSelectedYear(dividend.TaxYear));
+            System.IO.File.WriteAllText(filename, _dividendExportService.Export(filteredDividendSummary));
+        }
+    }
+}
