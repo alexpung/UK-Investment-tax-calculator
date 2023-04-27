31371613bfd6083ac4973211d24bb7434d63e8b8
Alex Pun <alexpung@protonmail.com>
2023-04-27 22:17:02 +0100

    Correct calculation where buy trade value is also counted

--- Model/TradeCalculationResult.cs
+++ Model/TradeCalculationResult.cs
@@ -15,8 +15,12 @@ public class TradeCalculationResult
 
     // Rounding to tax payer benefit https://www.gov.uk/hmrc-internal-manuals/self-assessment-manual/sam121370
     public int NumberOfDisposals(Func<TradeTaxCalculation, bool> filterCondition) => CalculatedTrade.Where(filterCondition).Count(trade => trade.BuySell == Enum.TradeType.SELL);
-    public int DisposalProceeds(Func<TradeTaxCalculation, bool> filterCondition) => (int)Math.Floor(CalculatedTrade.Where(filterCondition).Sum(trade => trade.TotalProceeds));
-    public int AllowableCosts(Func<TradeTaxCalculation, bool> filterCondition) => (int)Math.Ceiling(CalculatedTrade.Where(filterCondition).Sum(trade => trade.TotalAllowableCost));
-    public int TotalGain(Func<TradeTaxCalculation, bool> filterCondition) => (int)Math.Floor(CalculatedTrade.Where(filterCondition).Where(trade => trade.Gain > 0).Sum(trade => trade.Gain));
-    public int TotalLoss(Func<TradeTaxCalculation, bool> filterCondition) => (int)Math.Ceiling(CalculatedTrade.Where(filterCondition).Where(trade => trade.Gain < 0).Sum(trade => trade.Gain));
+    public int DisposalProceeds(Func<TradeTaxCalculation, bool> filterCondition) => (int)Math.Floor(CalculatedTrade.Where(filterCondition)
+        .Where(trade => trade.BuySell == Enum.TradeType.SELL).Sum(trade => trade.TotalProceeds));
+    public int AllowableCosts(Func<TradeTaxCalculation, bool> filterCondition) => (int)Math.Ceiling(CalculatedTrade.Where(filterCondition)
+        .Where(trade => trade.BuySell == Enum.TradeType.SELL).Sum(trade => trade.TotalAllowableCost));
+    public int TotalGain(Func<TradeTaxCalculation, bool> filterCondition) => (int)Math.Floor(CalculatedTrade.Where(filterCondition)
+        .Where(trade => trade.BuySell == Enum.TradeType.SELL).Where(trade => trade.Gain > 0).Sum(trade => trade.Gain));
+    public int TotalLoss(Func<TradeTaxCalculation, bool> filterCondition) => (int)Math.Ceiling(CalculatedTrade.Where(filterCondition)
+        .Where(trade => trade.BuySell == Enum.TradeType.SELL).Where(trade => trade.Gain < 0).Sum(trade => trade.Gain));
 }
