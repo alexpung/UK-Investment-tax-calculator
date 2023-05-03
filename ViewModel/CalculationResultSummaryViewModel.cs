07be0065e8a1e056b98d62de214715d528ff9013
Alex Pun <alexpung@protonmail.com>
2023-05-03 21:22:17 +0100

    Add dividend total ObservableProperties

--- ViewModel/CalculationResultSummaryViewModel.cs
+++ ViewModel/CalculationResultSummaryViewModel.cs
@@ -1,16 +1,14 @@
 ﻿using CapitalGainCalculator.Model;
-using CapitalGainCalculator.Model.Interfaces;
 using CapitalGainCalculator.ViewModel.Messages;
 using CapitalGainCalculator.ViewModel.Options;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CapitalGainCalculator.ViewModel;
 
-public partial class CalculationResultSummaryViewModel : ObservableRecipient, IRecipient<CalculationFinishedMessage>
+public partial class CalculationResultSummaryViewModel : ObservableRecipient, IRecipient<CalculationFinishedMessage>, IRecipient<YearSelectionChangedMessage>
 {
     private readonly TradeCalculationResult _tradeCalculationResult;
     private readonly DividendCalculationResult _dividendCalculationResult;
@@ -24,32 +22,31 @@ public partial class CalculationResultSummaryViewModel : ObservableRecipient, IR
     private decimal _totalGain;
     [ObservableProperty]
     private decimal _totalLoss;
+    [ObservableProperty]
+    private decimal _totalDividends;
+    [ObservableProperty]
+    private decimal _totalForeignTaxPaid;
     public YearOptions Years { get; init; }
-    private readonly ITaxYear _taxYear;
 
 
-    public CalculationResultSummaryViewModel(TradeCalculationResult tradeCalculationResult, DividendCalculationResult dividendCalculationResult, YearOptions years, ITaxYear taxYear)
+    public CalculationResultSummaryViewModel(TradeCalculationResult tradeCalculationResult, DividendCalculationResult dividendCalculationResult, YearOptions years)
     {
         _tradeCalculationResult = tradeCalculationResult;
         _dividendCalculationResult = dividendCalculationResult;
         Years = years;
-        Years.PropertyChanged += Years_PropertyChanged;
-        _taxYear = taxYear;
         IsActive = true;
     }
 
     public void Receive(CalculationFinishedMessage message)
-        {
+    {
         Years.SetYears(GetSelectableYears());
         Years.SelectAllYears();
-            UpdateSummary();
-        }
+        UpdateSummary();
     }
 
     public void Receive(YearSelectionChangedMessage message)
     {
         UpdateSummary();
-        Years.SetYears(GetSelectableYears());
     }
 
     private void UpdateSummary()
