ec358501d48e4d56a292bc26d7090ee6c3c81eef
Alex Pun <alexpung@protonmail.com>
2023-05-09 23:55:57 +0100

    Add method to retrieve list of section 104 pools

--- Model/UkTaxModel/UkSection104Pools.cs
+++ Model/UkTaxModel/UkSection104Pools.cs
@@ -7,6 +7,11 @@ public class UkSection104Pools
 {
     private readonly Dictionary<string, UkSection104> _section104Pools = new();
 
+    public List<UkSection104> GetSection104s()
+    {
+        return _section104Pools.Values.ToList();
+    }
+
     /// <summary>
     /// If the section104 pool does not exist, create a new one and return the initialised pool, otherwise return the existing pool
     /// </summary>
