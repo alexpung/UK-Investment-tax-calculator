fc28a1dce534cdf3fa63e5148759cdc8af6d08df
Alex Pun <alexpung@protonmail.com>
2023-06-16 21:00:08 +0100

    use DI instead of making the objects yourself

--- BlazorApp-Investment Tax Calculator/Parser/InteractiveBrokersXml/IBParseController.cs
+++ BlazorApp-Investment Tax Calculator/Parser/InteractiveBrokersXml/IBParseController.cs
@@ -6,22 +6,27 @@ namespace Parser.InteractiveBrokersXml;
 public class IBParseController : ITaxEventFileParser
 {
     private readonly AssetTypeToLoadSetting _assetTypeToLoadSetting;
-    public IBParseController(AssetTypeToLoadSetting assetTypeToLoadSetting)
+    private readonly IBXmlDividendParser _dividendParser;
+    private readonly IBXmlStockTradeParser _stockTradeParser;
+    private readonly IBXmlStockSplitParser _stockSplitParser;
+
+    public IBParseController(AssetTypeToLoadSetting assetTypeToLoadSetting, IBXmlDividendParser iBXmlDividendParser, IBXmlStockTradeParser iBXmlStockTradeParser, IBXmlStockSplitParser iBXmlStockSplitParser)
     {
         _assetTypeToLoadSetting = assetTypeToLoadSetting;
+        _dividendParser = iBXmlDividendParser;
+        _stockSplitParser = iBXmlStockSplitParser;
+        _stockTradeParser = iBXmlStockTradeParser;
+
     }
     public TaxEventLists ParseFile(string data)
     {
         TaxEventLists result = new();
-        IBXmlDividendParser dividendParser = new();
-        IBXmlStockSplitParser stockSplitParser = new();
-        IBXmlStockTradeParser stockTradeParser = new();
         XElement? xml = XDocument.Parse(data).Root;
         if (xml is not null)
         {
-            if (_assetTypeToLoadSetting.LoadDividends) result.Dividends.AddRange(dividendParser.ParseXml(xml));
-            if (_assetTypeToLoadSetting.LoadStocks) result.CorporateActions.AddRange(stockSplitParser.ParseXml(xml));
-            if (_assetTypeToLoadSetting.LoadStocks) result.Trades.AddRange(stockTradeParser.ParseXml(xml));
+            if (_assetTypeToLoadSetting.LoadDividends) result.Dividends.AddRange(_dividendParser.ParseXml(xml));
+            if (_assetTypeToLoadSetting.LoadStocks) result.CorporateActions.AddRange(_stockSplitParser.ParseXml(xml));
+            if (_assetTypeToLoadSetting.LoadStocks) result.Trades.AddRange(_stockTradeParser.ParseXml(xml));
         }
         return result;
     }
