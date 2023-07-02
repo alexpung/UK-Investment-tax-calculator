1a0607db7eb310ca694f524cf9baaaac2aafda06
Alex Pun <alexpung@protonmail.com>
2023-07-02 21:51:29 +0100

    Implement tests for DividendSummary

--- BlazorApp-Investment Tax Calculator/Model/DividendSummary.cs
+++ BlazorApp-Investment Tax Calculator/Model/DividendSummary.cs
@@ -7,13 +7,13 @@ namespace Model;
 public record DividendSummary
 {
     public required RegionInfo CountryOfOrigin { get; set; }
-    public required int TaxYear { get; set; }
+    public virtual required int TaxYear { get; set; }
     public required List<Dividend> RelatedDividendsAndTaxes { get; set; }
-    public Money TotalTaxableDividend => (from dividend in RelatedDividendsAndTaxes
-                                          where dividend.DividendType is DividendType.DIVIDEND_IN_LIEU or DividendType.DIVIDEND
-                                          select dividend.Proceed.BaseCurrencyAmount)
+    public virtual Money TotalTaxableDividend => (from dividend in RelatedDividendsAndTaxes
+                                                  where dividend.DividendType is DividendType.DIVIDEND_IN_LIEU or DividendType.DIVIDEND
+                                                  select dividend.Proceed.BaseCurrencyAmount)
                                             .Sum();
-    public Money TotalForeignTaxPaid => (from dividend in RelatedDividendsAndTaxes
-                                         where dividend.DividendType is DividendType.WITHHOLDING
-                                         select dividend.Proceed.BaseCurrencyAmount).Sum();
+    public virtual Money TotalForeignTaxPaid => (from dividend in RelatedDividendsAndTaxes
+                                                 where dividend.DividendType is DividendType.WITHHOLDING
+                                                 select dividend.Proceed.BaseCurrencyAmount).Sum();
 }
---
+++ UnitTest/Test/Model/DividendCalculationResultTest.cs
@@ -0,0 +1,80 @@
+using Moq;
+using NMoneys;
+
+namespace Model.Tests;
+
+public class DividendCalculationResultTests
+{
+    [Fact]
+    public void GetTotalDividend_ReturnsCorrectTotalDividend()
+    {
+        // Arrange
+        Mock<DividendSummary> dividendSummaryMock1 = new();
+        Mock<DividendSummary> dividendSummaryMock2 = new();
+        Mock<DividendSummary> dividendSummaryMock3 = new();
+        dividendSummaryMock1.Setup(i => i.TotalTaxableDividend).Returns(new Money(100, Currency.Gbp));
+        dividendSummaryMock2.Setup(i => i.TotalTaxableDividend).Returns(new Money(200, Currency.Gbp));
+        dividendSummaryMock3.Setup(i => i.TotalTaxableDividend).Returns(new Money(300, Currency.Gbp));
+        dividendSummaryMock1.Setup(i => i.TaxYear).Returns(2021);
+        dividendSummaryMock2.Setup(i => i.TaxYear).Returns(2022);
+        dividendSummaryMock3.Setup(i => i.TaxYear).Returns(2023);
+        var result = new DividendCalculationResult();
+        result.SetResult(new List<DividendSummary>() { dividendSummaryMock1.Object, dividendSummaryMock2.Object, dividendSummaryMock3.Object });
+        var yearFilter = new List<int> { 2021, 2023 };
+
+        // Act
+        var totalDividend = result.GetTotalDividend(yearFilter);
+
+        // Assert
+        Assert.Equal(new Money(400, Currency.Gbp), totalDividend);
+    }
+
+    [Fact]
+    public void GetForeignTaxPaid_ReturnsCorrectForeignTaxPaid()
+    {
+        // Arrange
+        Mock<DividendSummary> dividendSummaryMock1 = new();
+        Mock<DividendSummary> dividendSummaryMock2 = new();
+        Mock<DividendSummary> dividendSummaryMock3 = new();
+        dividendSummaryMock1.Setup(i => i.TotalForeignTaxPaid).Returns(new Money(30, Currency.Gbp));
+        dividendSummaryMock2.Setup(i => i.TotalForeignTaxPaid).Returns(new Money(50, Currency.Gbp));
+        dividendSummaryMock3.Setup(i => i.TotalForeignTaxPaid).Returns(new Money(70, Currency.Gbp));
+        dividendSummaryMock1.Setup(i => i.TaxYear).Returns(2021);
+        dividendSummaryMock2.Setup(i => i.TaxYear).Returns(2022);
+        dividendSummaryMock3.Setup(i => i.TaxYear).Returns(2023);
+        var result = new DividendCalculationResult();
+        result.SetResult(new List<DividendSummary>() { dividendSummaryMock1.Object, dividendSummaryMock2.Object, dividendSummaryMock3.Object });
+        var yearFilter = new List<int> { 2022, 2023 };
+
+        // Act
+        var foreignTaxPaid = result.GetForeignTaxPaid(yearFilter);
+
+        // Assert
+        Assert.Equal(new Money(120, Currency.Gbp), foreignTaxPaid);
+    }
+
+    [Fact]
+    public void TestNoYearSelected()
+    {
+        // Arrange
+        Mock<DividendSummary> dividendSummaryMock1 = new();
+        Mock<DividendSummary> dividendSummaryMock2 = new();
+        Mock<DividendSummary> dividendSummaryMock3 = new();
+        dividendSummaryMock1.Setup(i => i.TotalForeignTaxPaid).Returns(new Money(30, Currency.Gbp));
+        dividendSummaryMock2.Setup(i => i.TotalForeignTaxPaid).Returns(new Money(50, Currency.Gbp));
+        dividendSummaryMock3.Setup(i => i.TotalForeignTaxPaid).Returns(new Money(70, Currency.Gbp));
+        dividendSummaryMock1.Setup(i => i.TaxYear).Returns(2021);
+        dividendSummaryMock2.Setup(i => i.TaxYear).Returns(2022);
+        dividendSummaryMock3.Setup(i => i.TaxYear).Returns(2023);
+        var result = new DividendCalculationResult();
+        result.SetResult(new List<DividendSummary>() { dividendSummaryMock1.Object, dividendSummaryMock2.Object, dividendSummaryMock3.Object });
+        var yearFilter = new List<int>();
+
+        // Act
+        var foreignTaxPaid = result.GetForeignTaxPaid(yearFilter);
+
+        // Assert
+        Assert.Equal(BaseCurrencyMoney.BaseCurrencyZero, foreignTaxPaid);
+    }
+}
+
