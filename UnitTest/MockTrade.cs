49acf75ec314b4e3217618295fc87a92c6272229
Alex Pun <alexpung@protonmail.com>
2023-07-02 20:52:04 +0100

    Fix bug in Mock trade when correct value in base currency is not used

--- UnitTest/MockTrade.cs
+++ UnitTest/MockTrade.cs
@@ -2,20 +2,19 @@
 using Model;
 using Model.Interfaces;
 using Moq;
-using NMoneys;
 
 namespace UnitTest;
 public static class MockTrade
 {
-    public static Mock<ITradeTaxCalculation> CreateMockTrade(decimal quantity, Money value, TradeType tradeType)
+    public static Mock<ITradeTaxCalculation> CreateMockTrade(decimal quantity, decimal value, TradeType tradeType)
     {
         Mock<ITradeTaxCalculation> mockTrade = new();
-        mockTrade.Setup(f => f.MatchAll()).Returns((quantity, value));
+        mockTrade.Setup(f => f.MatchAll()).Returns((quantity, BaseCurrencyMoney.BaseCurrencyAmount(value)));
         mockTrade.Setup(f => f.BuySell).Returns(tradeType);
         mockTrade.Setup(f => f.MatchHistory).Returns(new List<TradeMatch>());
         mockTrade.Setup(f => f.UnmatchedQty).Returns(quantity);
         mockTrade.Setup(f => f.TradeList).Returns(new List<Trade>());
-        mockTrade.Setup(f => f.MatchQty(It.IsAny<decimal>())).Returns<decimal>(x => (x, value.Multiply(x).Divide(quantity)));
+        mockTrade.Setup(f => f.MatchQty(It.IsAny<decimal>())).Returns<decimal>(x => (x, BaseCurrencyMoney.BaseCurrencyAmount(value * x / quantity)));
         return mockTrade;
     }
 }
--- UnitTest/Test/UkSection104Test.cs
+++ UnitTest/Test/UkSection104Test.cs
@@ -4,7 +4,6 @@ using Model.Interfaces;
 using Model.UkTaxModel;
 using Moq;
 using Shouldly;
-using UnitTest;
 
 namespace UnitTest.Test;
 
@@ -16,7 +15,7 @@ public class UkSection104Test
     [InlineData(100, 1000, 150, 4000)]
     public void TestAddandRemoveSection104(decimal buyQuantity, decimal buyValue, decimal sellQuantity, decimal sellValue)
     {
-        Mock<ITradeTaxCalculation> mockBuyTrade = MockTrade.CreateMockTrade(buyQuantity, buyValue.ConvertToBaseCurrency(), TradeType.BUY);
+        Mock<ITradeTaxCalculation> mockBuyTrade = MockTrade.CreateMockTrade(buyQuantity, buyValue, TradeType.BUY);
         UkSection104 ukSection104 = new("IBM");
         ukSection104.MatchTradeWithSection104(mockBuyTrade.Object);
         ukSection104.AssetName.ShouldBe("IBM");
@@ -26,7 +25,7 @@ public class UkSection104Test
         mockBuyTrade.Object.MatchHistory[0].BaseCurrencyMatchAcquitionValue.ShouldBe(buyValue.ConvertToBaseCurrency());
         mockBuyTrade.Object.MatchHistory[0].BaseCurrencyMatchDisposalValue.ShouldBe(BaseCurrencyMoney.BaseCurrencyZero);
         mockBuyTrade.Object.MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.SECTION_104);
-        Mock<ITradeTaxCalculation> mockSellTrade = MockTrade.CreateMockTrade(sellQuantity, sellValue.ConvertToBaseCurrency(), TradeType.SELL);
+        Mock<ITradeTaxCalculation> mockSellTrade = MockTrade.CreateMockTrade(sellQuantity, sellValue, TradeType.SELL);
         ukSection104.MatchTradeWithSection104(mockSellTrade.Object);
         ukSection104.Quantity.ShouldBe(decimal.Max(buyQuantity - sellQuantity, 0));
         ukSection104.ValueInBaseCurrency.ShouldBe(decimal.Max((buyQuantity - sellQuantity) / buyQuantity * buyValue, 0).ConvertToBaseCurrency());
@@ -39,10 +38,10 @@ public class UkSection104Test
     [Fact]
     public void TestSection104History()
     {
-        Mock<ITradeTaxCalculation> mockTrade1 = MockTrade.CreateMockTrade(100, 1000m.ConvertToBaseCurrency(), TradeType.BUY);
-        Mock<ITradeTaxCalculation> mockTrade2 = MockTrade.CreateMockTrade(200, 2000m.ConvertToBaseCurrency(), TradeType.BUY);
-        Mock<ITradeTaxCalculation> mockTrade3 = MockTrade.CreateMockTrade(300, 3000m.ConvertToBaseCurrency(), TradeType.BUY);
-        Mock<ITradeTaxCalculation> mockTrade4 = MockTrade.CreateMockTrade(400, 8000m.ConvertToBaseCurrency(), TradeType.SELL);
+        Mock<ITradeTaxCalculation> mockTrade1 = MockTrade.CreateMockTrade(100, 1000m, TradeType.BUY);
+        Mock<ITradeTaxCalculation> mockTrade2 = MockTrade.CreateMockTrade(200, 2000m, TradeType.BUY);
+        Mock<ITradeTaxCalculation> mockTrade3 = MockTrade.CreateMockTrade(300, 3000m, TradeType.BUY);
+        Mock<ITradeTaxCalculation> mockTrade4 = MockTrade.CreateMockTrade(400, 8000m, TradeType.SELL);
         UkSection104 ukSection104 = new("IBM");
         ukSection104.MatchTradeWithSection104(mockTrade1.Object);
         ukSection104.MatchTradeWithSection104(mockTrade2.Object);
@@ -69,8 +68,8 @@ public class UkSection104Test
     [Fact]
     public void TestSection104HandleShareSplit()
     {
-        Mock<ITradeTaxCalculation> mockTrade1 = MockTrade.CreateMockTrade(100, 1000m.ConvertToBaseCurrency(), TradeType.BUY);
-        Mock<ITradeTaxCalculation> mockTrade2 = MockTrade.CreateMockTrade(120, 1500m.ConvertToBaseCurrency(), TradeType.SELL);
+        Mock<ITradeTaxCalculation> mockTrade1 = MockTrade.CreateMockTrade(100, 1000m, TradeType.BUY);
+        Mock<ITradeTaxCalculation> mockTrade2 = MockTrade.CreateMockTrade(120, 1500m, TradeType.SELL);
         CorporateAction corporateAction = new StockSplit() { AssetName = "ABC", Date = new DateTime(), NumberAfterSplit = 3, NumberBeforeSplit = 2 };
         UkSection104 ukSection104 = new("IBM");
         ukSection104.MatchTradeWithSection104(mockTrade1.Object);
