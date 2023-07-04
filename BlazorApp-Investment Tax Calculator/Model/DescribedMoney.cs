50032f7bf71fe35c395fc967ed46bc0a2500ef8c
Alex Pun <alexpung@protonmail.com>
2023-07-04 19:46:23 +0100

    Move Money extension method to a new file

--- BlazorApp-Investment Tax Calculator/Model/DescribedMoney.cs
+++ BlazorApp-Investment Tax Calculator/Model/DescribedMoney.cs
@@ -10,25 +10,3 @@ public record DescribedMoney
 
     public Money BaseCurrencyAmount => BaseCurrencyMoney.BaseCurrencyAmount(Amount.Amount * FxRate);
 }
-
-public static class DescribedMoneyExtension
-{
-    public static Money BaseCurrencySum(this IEnumerable<Money> moneys)
-    {
-        if (!moneys.Any()) return BaseCurrencyMoney.BaseCurrencyZero;
-        return Money.Total(moneys);
-    }
-
-    /// <summary>
-    /// Sum up an IEnumerable of objects that contain Money object. Default return Money of Amount 0 in base currency.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="moneys"></param>
-    /// <param name="selector">Delegate to select money object</param>
-    /// <returns></returns>
-    public static Money BaseCurrencySum<T>(this IEnumerable<T> moneys, Func<T, Money> selector)
-    {
-        if (!moneys.Any()) return BaseCurrencyMoney.BaseCurrencyZero;
-        return Money.Total(moneys.Select(selector));
-    }
-}
