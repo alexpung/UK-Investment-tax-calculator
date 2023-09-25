34a554a86478a1119e03db28b96b65f4a2fae001
Alex Pun <alexpung@protonmail.com>
2023-09-25 20:14:19 +0100

    Correct GrossProceed formatting in text file export

--- BlazorApp-Investment Tax Calculator/Model/Trade.cs
+++ BlazorApp-Investment Tax Calculator/Model/Trade.cs
@@ -47,7 +47,7 @@ public record Trade : TaxEvent, ITextFilePrintable
             TradeType.SELL => $"Net proceed: {NetProceed}",
             _ => throw new NotImplementedException()
         };
-        return $"{action} {Quantity} unit(s) of {AssetName} on {Date:dd-MMM-yyyy} for {GrossProceed} " +
+        return $"{action} {Quantity} unit(s) of {AssetName} on {Date:dd-MMM-yyyy} for {GrossProceed.PrintToTextFile()} " +
             $"with total expense {Expenses.Sum(expenses => expenses.BaseCurrencyAmount)}, {netExplanation}"
             + GetExpensesExplanation();
     }
