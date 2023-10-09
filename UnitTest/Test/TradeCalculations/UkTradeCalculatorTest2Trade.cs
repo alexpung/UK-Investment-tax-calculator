55fb3a65f0924c88abc3c3ff82f12e0e306c4b86
Alex Pun <alexpung@protonmail.com>
2023-10-09 22:50:27 +0100

    Update calculationt tests

---
+++ UnitTest/Test/TradeCalculations/UkTradeCalculatorTest2Trade.cs
@@ -0,0 +1,192 @@
+﻿using Model;
+using Model.Interfaces;
+using Model.UkTaxModel;
+
+namespace UnitTest.Test.TradeCalculations;
+public class UkTradeCalculatorTest2Trade
+{
+    [Fact]
+    public void TestSimpleShortCover()
+    {
+        Trade trade1 = new()
+        {
+            AssetName = "DEF",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("05-May-21 12:34:56"),
+            Description = "DEF Example Stock",
+            Quantity = 100,
+            Expenses = new() { new() { Description = "Commission", Amount = new(1.5m, "USD"), FxRate = 0.86m } },
+            GrossProceed = new() { Description = "Commission", Amount = new(1000m, "USD"), FxRate = 0.86m },
+        };
+        Trade trade2 = new()
+        {
+            AssetName = "DEF",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("06-Dec-21 12:34:56"),
+            Description = "DEF Example Stock",
+            Quantity = 100,
+            Expenses = new() { new() { Description = "Commission", Amount = new(1.5m, "USD"), FxRate = 0.86m } },
+            GrossProceed = new() { Description = "", Amount = new(500m, "USD"), FxRate = 0.86m },
+        };
+        UkSection104Pools section104Pools = new();
+        TaxEventLists taxEventLists = new TaxEventLists();
+        taxEventLists.AddData(new List<Trade>() { trade1, trade2 });
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+        result[0].Gain.ShouldBe(new WrappedMoney(427.42m));
+        result[0].TotalAllowableCost.ShouldBe(new WrappedMoney(431.29m));
+        result[0].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.SHORTCOVER);
+        section104Pools.GetExistingOrInitialise("DEF").ValueInBaseCurrency.ShouldBe(WrappedMoney.GetBaseCurrencyZero());
+        section104Pools.GetExistingOrInitialise("DEF").Quantity.ShouldBe(0);
+    }
+
+
+
+
+    [Fact]
+    public void TestSimpleSection104()
+    {
+        // Create a trade representing the purchase of an asset
+        Trade buyTrade = new()
+        {
+            AssetName = "ABC",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("01-Jan-21 10:00:00"),
+            Description = "Purchase of ABC Stock",
+            Quantity = 200,
+            Expenses = new() { new() { Description = "Commission", Amount = new(2.0m, "USD"), FxRate = 0.85m } },
+            GrossProceed = new() { Description = "", Amount = new(2000m, "USD"), FxRate = 0.85m },
+        };
+
+        // Create a trade representing the sale of the same asset
+        Trade sellTrade = new()
+        {
+            AssetName = "ABC",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("15-Dec-21 15:30:00"),
+            Description = "Sale of ABC Stock",
+            Quantity = 150,
+            Expenses = new() { new() { Description = "Commission", Amount = new(1.5m, "USD"), FxRate = 0.85m } },
+            GrossProceed = new() { Description = "", Amount = new(2250m, "USD"), FxRate = 0.85m },
+        };
+
+        // Create a Section104Pools instance
+        UkSection104Pools section104Pools = new();
+
+        // Create a TaxEventLists instance and add the trades to it
+        TaxEventLists taxEventLists = new TaxEventLists();
+        taxEventLists.AddData(new List<Trade>() { buyTrade, sellTrade });
+
+        // Create the UkTradeCalculator instance
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+
+        // Calculate the tax
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+
+        // Assert the expected results
+        result[1].Gain.ShouldBe(new WrappedMoney(634.95m)); // (2250 - 1.5 - (2000 + 2) * (150 / 200)) * 0.85
+        result[1].TotalAllowableCost.ShouldBe(new WrappedMoney(1276.275m)); // (2000 + 2) * (150 / 200) * 0.85
+        result[1].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.SECTION_104);
+
+        // Assert that Section 104 pool is updated correctly
+        section104Pools.GetExistingOrInitialise("ABC").ValueInBaseCurrency.ShouldBe(new WrappedMoney(425.425m)); // (2000 + 2) * (50 / 200) * 0.85
+        section104Pools.GetExistingOrInitialise("ABC").Quantity.ShouldBe(50); // 200 - 150
+    }
+
+    [Fact]
+    public void TestSimpleSameDayMatching()
+    {
+        // Create two trades representing a same-day purchase and sale of the same asset
+        Trade purchaseTrade = new()
+        {
+            AssetName = "XYZ",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("02-Feb-22 09:30:00"),
+            Description = "Purchase of XYZ Stock",
+            Quantity = 100,
+            Expenses = new() { new() { Description = "Commission", Amount = new(1.0m, "USD"), FxRate = 0.88m } },
+            GrossProceed = new() { Description = "", Amount = new(1000m, "USD"), FxRate = 0.88m },
+        };
+
+        Trade saleTrade = new()
+        {
+            AssetName = "XYZ",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("02-Feb-22 15:45:00"),
+            Description = "Sale of XYZ Stock",
+            Quantity = 100,
+            Expenses = new() { new() { Description = "Commission", Amount = new(1.0m, "USD"), FxRate = 0.85m } },
+            GrossProceed = new() { Description = "", Amount = new(1100m, "USD"), FxRate = 0.85m },
+        };
+
+        // Create a Section104Pools instance
+        UkSection104Pools section104Pools = new();
+
+        // Create a TaxEventLists instance and add the trades to it
+        TaxEventLists taxEventLists = new TaxEventLists();
+        taxEventLists.AddData(new List<Trade>() { purchaseTrade, saleTrade });
+
+        // Create the UkTradeCalculator instance
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+
+        // Calculate the tax
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+
+        // Assert the expected results
+        result[0].Gain.ShouldBe(new WrappedMoney(53.27m)); // (1100 - 1) * 0.85 - (1000 + 1) * 0.88
+        result[0].TotalAllowableCost.ShouldBe(new WrappedMoney(880.88m)); // (1000 + 1) * 0.88
+        result[0].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.SAME_DAY);
+
+        // Assert that Section 104 pool is updated correctly
+        section104Pools.GetExistingOrInitialise("XYZ").ValueInBaseCurrency.ShouldBe(new WrappedMoney(0m));
+        section104Pools.GetExistingOrInitialise("XYZ").Quantity.ShouldBe(0);
+    }
+
+    [Fact]
+    public void TestShortBedAndBreakfast()
+    {
+        Trade saleTrade = new()
+        {
+            AssetName = "BBB",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("12-Apr-22 15:45:00"),
+            Description = "Sale of BBB Stock",
+            Quantity = 100,
+            Expenses = new() { new() { Description = "Commission", Amount = new(1.0m, "USD"), FxRate = 0.88m } }, // Different FX rate
+            GrossProceed = new() { Description = "", Amount = new(1100m, "USD"), FxRate = 0.88m }, // Different FX rate
+        };
+
+        Trade purchaseTrade1 = new()
+        {
+            AssetName = "BBB",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("12-May-22 09:30:00"),
+            Description = "Purchase of BBB Stock",
+            Quantity = 200,
+            Expenses = new() { new() { Description = "Commission", Amount = new(2.0m, "USD"), FxRate = 0.85m } },
+            GrossProceed = new() { Description = "", Amount = new(2500m, "USD"), FxRate = 0.85m },
+        };
+
+        // Create a Section104Pools instance
+        UkSection104Pools section104Pools = new();
+
+        // Create a TaxEventLists instance and add the trades to it
+        TaxEventLists taxEventLists = new();
+        taxEventLists.AddData(new List<Trade>() { purchaseTrade1, saleTrade });
+
+        // Create the UkTradeCalculator instance
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+
+        // Calculate the tax
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+
+        // Assert the expected results for bed and breakfast matching with different FX rates
+        result[1].Gain.ShouldBe(new WrappedMoney(-96.23m)); // (1100 - 1) * 0.88 - (2500 + 2) * 100 / 200 * 0.85
+        result[1].TotalAllowableCost.ShouldBe(new WrappedMoney(1063.35m)); // (2500 + 2) * 100 / 200 * 0.85
+        result[1].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.BED_AND_BREAKFAST);
+
+        // Assert that Section 104 pool is updated correctly
+        section104Pools.GetExistingOrInitialise("BBB").ValueInBaseCurrency.ShouldBe(new WrappedMoney(1063.35m)); // (2500 + 2) * 100 / 200 * 0.85
+        section104Pools.GetExistingOrInitialise("BBB").Quantity.ShouldBe(100);
+    }
+}
---
+++ UnitTest/Test/TradeCalculations/UkTradeCalculatorTest3Trades.cs
@@ -0,0 +1,249 @@
+﻿using Model;
+using Model.Interfaces;
+using Model.UkTaxModel;
+
+namespace UnitTest.Test.TradeCalculations;
+public class UkTradeCalculatorTest3Trades
+{
+    /// <summary>
+    /// Test interaction of Bed and breakfast and short cover
+    /// </summary>
+    [Fact]
+    public void TestCaculateShortPartialCover()
+    {
+        Trade trade1 = new()
+        {
+            AssetName = "DEF",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("08-Apr-21 12:34:56"),
+            Description = "DEF Example Stock",
+            Quantity = 100,
+            GrossProceed = new() { Description = "Commission", Amount = new(1000m) },
+        };
+        Trade trade2 = new()
+        {
+            AssetName = "DEF",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("06-May-21 13:34:56"),
+            Description = "DEF Example Stock",
+            Quantity = 100,
+            GrossProceed = new() { Description = "Commission", Amount = new(1500m) },
+        };
+        Trade trade3 = new()
+        {
+            AssetName = "DEF",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("09-May-21 13:34:56"),
+            Description = "DEF Example Stock",
+            Quantity = 300,
+            GrossProceed = new() { Description = "", Amount = new(5000m) },
+        };
+        UkSection104Pools section104Pools = new();
+        TaxEventLists taxEventLists = new TaxEventLists();
+        taxEventLists.AddData(new List<Trade>() { trade1, trade2, trade3 });
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+        result[0].Gain.Amount.ShouldBe(-666.67m, 0.01m);
+        result[0].TotalAllowableCost.Amount.ShouldBe(1666.67m, 0.01m);
+        result[0].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.SHORTCOVER);
+        result[1].Gain.Amount.ShouldBe(-166.67m, 0.01m);
+        result[1].TotalAllowableCost.Amount.ShouldBe(1666.67m, 0.01m);
+        result[1].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.BED_AND_BREAKFAST);
+        section104Pools.GetExistingOrInitialise("DEF").ValueInBaseCurrency.ShouldBe(new WrappedMoney(1666.6666666666666666666666666m));
+        section104Pools.GetExistingOrInitialise("DEF").Quantity.ShouldBe(100);
+    }
+
+    [Fact]
+    public void TestBreadAndBreakfast3Trades()
+    {
+        Trade initSection104 = new()
+        {
+            AssetName = "Mesopotamia plc",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("11-Sep-20 12:00:00"),
+            Description = "Purchase of 9500 shares",
+            Quantity = 9500,
+            GrossProceed = new() { Description = "", Amount = new(1850.0m, "GBP"), FxRate = 1.0m },
+        };
+
+        Trade saleTrade1 = new()
+        {
+            AssetName = "Mesopotamia plc",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("30-Aug-21 14:00:00"),
+            Description = "Sale of 4,000 shares",
+            Quantity = 4000,
+            GrossProceed = new() { Description = "", Amount = new(6000.0m, "GBP"), FxRate = 1.0m },
+        };
+
+
+        // This buy trade as bed and breakfast
+        Trade purchaseTrade1 = new()
+        {
+            AssetName = "Mesopotamia plc",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("11-Sep-21 12:00:00"),
+            Description = "Purchase of 500 shares",
+            Quantity = 500,
+            GrossProceed = new() { Description = "", Amount = new(850.0m, "GBP"), FxRate = 1.0m },
+        };
+
+        // Create a Section104Pools instance
+        UkSection104Pools section104Pools = new();
+
+        // Create a TaxEventLists instance and add the trades to it
+        TaxEventLists taxEventLists = new();
+        taxEventLists.AddData(new List<Trade>() { initSection104, purchaseTrade1, saleTrade1 });
+
+        // Create the UkTradeCalculator instance
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+
+        // Calculate the tax
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+
+        // Assert the expected results
+        // Bed and Breakfast Matching (500 shares)
+        result[2].MatchHistory[0].BaseCurrencyMatchDisposalValue.ShouldBe(new WrappedMoney(750m));
+        result[2].MatchHistory[0].BaseCurrencyMatchAcquitionValue.ShouldBe(new WrappedMoney(850m));
+        result[2].MatchHistory[0].MatchQuantity.ShouldBe(500);
+        result[2].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.BED_AND_BREAKFAST);
+        // Section 104 Holding Matching (3,500 shares)
+        result[2].MatchHistory[1].BaseCurrencyMatchDisposalValue.ShouldBe(new WrappedMoney(5250m)); // (6000 * 3500 / 4000)
+        result[2].MatchHistory[1].BaseCurrencyMatchAcquitionValue.ShouldBe(new WrappedMoney(681.57894736842105263157894737m)); // (1850 * 3500 / 9500)
+        result[2].MatchHistory[1].MatchQuantity.ShouldBe(3500);
+        result[2].MatchHistory[1].TradeMatchType.ShouldBe(TaxMatchType.SECTION_104);
+
+        //Total
+        result[2].Gain.ShouldBe(new WrappedMoney(4468.4210526315789473684210526m)); // (6000 * 500 / 4000 - 850) + (6000 * 3500 / 4000) - (1850 * 3500 / 9500)
+        result[2].TotalAllowableCost.ShouldBe(new WrappedMoney(1531.5789473684210526315789474m));
+
+        // Ensure the Section 104 pool is updated correctly
+        section104Pools.GetExistingOrInitialise("Mesopotamia plc").ValueInBaseCurrency.ShouldBe(new WrappedMoney(1168.4210526315789473684210526m)); // 1850 - (1850 * 3500 / 9500)
+        section104Pools.GetExistingOrInitialise("Mesopotamia plc").Quantity.ShouldBe(6000);
+    }
+
+    [Fact]
+    public void TestSameDayAndBedAndBreakfastMatching()
+    {
+        // Create trades representing the scenario
+        Trade purchaseTrade1 = new()
+        {
+            AssetName = "ABC",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("01-Jan-22 10:00:00"),
+            Description = "Purchase of 100 shares",
+            Quantity = 100,
+            Expenses = new() { new() { Description = "Commission", Amount = new(2.0m, "GBP"), FxRate = 1.0m } },
+            GrossProceed = new() { Description = "", Amount = new(1000.0m, "GBP"), FxRate = 1.0m },
+        };
+
+        Trade saleTrade1 = new()
+        {
+            AssetName = "ABC",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("01-Jan-22 14:00:00"),
+            Description = "Sale of 200 shares (Same Day)",
+            Quantity = 200,
+            Expenses = new() { new() { Description = "Commission", Amount = new(1.0m, "GBP"), FxRate = 1.0m } },
+            GrossProceed = new() { Description = "", Amount = new(2200.0m, "GBP"), FxRate = 1.0m },
+        };
+
+        Trade purchaseTrade2 = new()
+        {
+            AssetName = "ABC",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("01-Feb-22 10:00:00"), // Within 30 days
+            Description = "Purchase of 100 shares (Bed and Breakfast)",
+            Quantity = 100,
+            Expenses = new() { new() { Description = "Commission", Amount = new(2.0m, "GBP"), FxRate = 1.0m } },
+            GrossProceed = new() { Description = "", Amount = new(1100.0m, "GBP"), FxRate = 1.0m },
+        };
+
+        // Create a TaxEventLists instance and add the trades to it
+        TaxEventLists taxEventLists = new TaxEventLists();
+        taxEventLists.AddData(new List<Trade>() { purchaseTrade1, saleTrade1, purchaseTrade2 });
+
+        // Initialize the Section 104 pools (even though they are not used in calculations)
+        UkSection104Pools section104Pools = new();
+
+        // Create the UkTradeCalculator instance
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+
+        // Calculate the tax
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+
+        // Assert the expected results
+        // Same Day Matching (SaleTrade1)
+        decimal gainFromOwnedShares = (10.995m * 100) - (10.02m * 100); // £97.5
+        decimal lossFromShortCover = (11.02m * 100) - (10.995m * 100);  // £2.5
+        decimal totalGain = gainFromOwnedShares - lossFromShortCover;   // £95
+
+        result[1].Gain.Amount.ShouldBe(totalGain, 0.01m);
+        result[1].TotalAllowableCost.Amount.ShouldBe(2104.0m, 0.01m); // Cost basis for owned shares
+        result[1].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.SAME_DAY);
+        result[2].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.SHORTCOVER);
+    }
+
+    [Fact]
+    public void TestMultipleTradesWithPooling()
+    {
+        Trade initialPurchase = new()
+        {
+            AssetName = "AncientArtifacts Ltd",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("15-Jan-20 10:00:00"),
+            Description = "Purchase of 10,000 shares",
+            Quantity = 10000,
+            GrossProceed = new() { Description = "", Amount = new(2000.0m, "GBP"), FxRate = 1.0m },
+        };
+
+        Trade saleTrade = new()
+        {
+            AssetName = "AncientArtifacts Ltd",
+            BuySell = Enum.TradeType.SELL,
+            Date = DateTime.Parse("20-Aug-21 11:00:00"),
+            Description = "Sale of 3,000 shares",
+            Quantity = 3000,
+            GrossProceed = new() { Description = "", Amount = new(2500.0m, "GBP"), FxRate = 1.0m },
+        };
+
+        Trade additionalPurchase = new()
+        {
+            AssetName = "AncientArtifacts Ltd",
+            BuySell = Enum.TradeType.BUY,
+            Date = DateTime.Parse("01-Sep-21 15:00:00"),
+            Description = "Purchase of 2,000 shares",
+            Quantity = 2000,
+            GrossProceed = new() { Description = "", Amount = new(1500.0m, "GBP"), FxRate = 1.0m },
+        };
+
+        UkSection104Pools section104Pools = new();
+        TaxEventLists taxEventLists = new();
+        taxEventLists.AddData(new List<Trade>() { initialPurchase, saleTrade, additionalPurchase });
+
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+
+        // Assert the expected results
+        // Short Cover Matching (2000 shares)
+        result[1].MatchHistory[0].BaseCurrencyMatchDisposalValue.Amount.ShouldBe(1666.67m, 0.01m);
+        result[1].MatchHistory[0].BaseCurrencyMatchAcquitionValue.Amount.ShouldBe(1500m, 0.01m);
+        result[1].MatchHistory[0].MatchQuantity.ShouldBe(2000);
+        result[1].MatchHistory[0].TradeMatchType.ShouldBe(TaxMatchType.BED_AND_BREAKFAST);
+
+        // Section 104 Holding Matching (1000 shares)
+        result[1].MatchHistory[1].BaseCurrencyMatchDisposalValue.Amount.ShouldBe(833.33m, 0.01m);
+        result[1].MatchHistory[1].BaseCurrencyMatchAcquitionValue.Amount.ShouldBe(200m, 0.01m);
+        result[1].MatchHistory[1].MatchQuantity.ShouldBe(1000);
+        result[1].MatchHistory[1].TradeMatchType.ShouldBe(TaxMatchType.SECTION_104);
+
+        //Total
+        result[1].Gain.Amount.ShouldBe(800m, 0.01m);
+        result[1].TotalAllowableCost.Amount.ShouldBe(1700m, 0.01m);
+
+        // Ensure the Section 104 pool is updated correctly
+        section104Pools.GetExistingOrInitialise("AncientArtifacts Ltd").ValueInBaseCurrency.Amount.ShouldBe(1800m, 0.01m);
+        section104Pools.GetExistingOrInitialise("AncientArtifacts Ltd").Quantity.ShouldBe(9000);
+    }
+}
