f0e426a9233b471b258b22b3e9bccf237d835a1c
Alex Pun <alexpung@protonmail.com>
2023-10-15 20:22:20 +0100

    Mark private helper method in IBXmlStockTradeParser static

--- BlazorApp-Investment Tax Calculator/Parser/InteractiveBrokersXml/IBXmlStockTradeParser.cs
+++ BlazorApp-Investment Tax Calculator/Parser/InteractiveBrokersXml/IBXmlStockTradeParser.cs
@@ -30,28 +30,28 @@ public class IBXmlStockTradeParser
         catch { return null; } // TODO Implement suitable catch clause and logging
     }
 
-    private decimal GetQuantity(XElement element) => element.GetAttribute("buySell") switch
+    private static decimal GetQuantity(XElement element) => element.GetAttribute("buySell") switch
     {
         "BUY" => decimal.Parse(element.GetAttribute("quantity")),
         "SELL" => decimal.Parse(element.GetAttribute("quantity")) * -1,
         _ => throw new NotImplementedException(),
     };
 
-    private DescribedMoney GetGrossProceed(XElement element) => element.GetAttribute("buySell") switch
+    private static DescribedMoney GetGrossProceed(XElement element) => element.GetAttribute("buySell") switch
     {
         "BUY" => element.BuildDescribedMoney("proceeds", "currency", "fxRateToBase", "", true),
         "SELL" => element.BuildDescribedMoney("proceeds", "currency", "fxRateToBase", ""),
         _ => throw new NotImplementedException(),
     };
 
-    private TradeType GetTradeType(XElement element) => element.GetAttribute("buySell") switch
+    private static TradeType GetTradeType(XElement element) => element.GetAttribute("buySell") switch
     {
         "BUY" => TradeType.BUY,
         "SELL" => TradeType.SELL,
         _ => throw new NotImplementedException($"Unrecognised trade type {element.GetAttribute("buySell")}")
     };
 
-    private List<DescribedMoney> BuildExpenses(XElement element)
+    private static List<DescribedMoney> BuildExpenses(XElement element)
     {
         List<DescribedMoney> expenses = new List<DescribedMoney>();
         if (element.GetAttribute("ibCommission") != "0")
