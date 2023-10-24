d31fd530511345097fe399cdb4ff77bc2385d89c
Alex Pun <alexpung@protonmail.com>
2023-10-24 22:57:38 +0100

    Add hmrc example of cgt calculation

--- UnitTest/Test/TradeCalculations/UkTradeCalculatorTest4Trades.cs
+++ UnitTest/Test/TradeCalculations/UkTradeCalculatorTest4Trades.cs
@@ -13,7 +13,7 @@ public class UkTradeCalculatorTest4Trades
         Trade trade1 = new()
         {
             AssetName = "ABC",
-            BuySell = Enum.TradeType.BUY,
+            BuySell = TradeType.BUY,
             Date = DateTime.Parse("01-May-21 12:34:56"),
             Description = "ABC Example Stock",
             Quantity = 200,
@@ -23,7 +23,7 @@ public class UkTradeCalculatorTest4Trades
         Trade trade2 = new()
         {
             AssetName = "ABC",
-            BuySell = Enum.TradeType.BUY,
+            BuySell = TradeType.BUY,
             Date = DateTime.Parse("03-May-21 12:33:56"),
             Description = "ABC Example Stock",
             Quantity = 50,
@@ -33,7 +33,7 @@ public class UkTradeCalculatorTest4Trades
         Trade trade3 = new()
         {
             AssetName = "ABC",
-            BuySell = Enum.TradeType.SELL,
+            BuySell = TradeType.SELL,
             Date = DateTime.Parse("03-May-21 12:34:56"),
             Description = "ABC Example Stock",
             Quantity = 200,
@@ -43,7 +43,7 @@ public class UkTradeCalculatorTest4Trades
         Trade trade4 = new()
         {
             AssetName = "ABC",
-            BuySell = Enum.TradeType.BUY,
+            BuySell = TradeType.BUY,
             Date = DateTime.Parse("04-May-21 12:34:56"),
             Description = "ABC Example Stock",
             Quantity = 100,
@@ -75,5 +75,61 @@ public class UkTradeCalculatorTest4Trades
         result[2].MatchHistory[2].BaseCurrencyMatchAcquitionValue.ShouldBe(new WrappedMoney(808.6m));
         section104Pools.GetExistingOrInitialise("ABC").ValueInBaseCurrency.ShouldBe(new WrappedMoney(808.6m));
         section104Pools.GetExistingOrInitialise("ABC").Quantity.ShouldBe(200);
+    }
+
+    // Example from https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1145311/HS284_Example_3_2023.pdf
+    [Fact]
+    public void HMRCExample()
+    {
+        Trade trade1 = new()
+        {
+            AssetName = "Lobster plc",
+            BuySell = TradeType.BUY,
+            Date = DateTime.Parse("01-Apr-14 12:33:56"),
+            Description = "Lobster plc",
+            Quantity = 1000,
+            Expenses = new() { new() { Description = "Commission", Amount = new(150m) } },
+            GrossProceed = new() { Amount = new(4000m) },
+        };
+
+        Trade trade2 = new()
+        {
+            AssetName = "Lobster plc",
+            BuySell = TradeType.BUY,
+            Date = DateTime.Parse("01-Sep-17 12:33:56"),
+            Description = "Lobster plc",
+            Quantity = 500,
+            Expenses = new() { new() { Description = "Commission", Amount = new(80m) } },
+            GrossProceed = new() { Amount = new(2050m) },
+        };
+
+        Trade trade3 = new()
+        {
+            AssetName = "Lobster plc",
+            BuySell = TradeType.SELL,
+            Date = DateTime.Parse("01-May-22 12:33:56"),
+            Description = "Lobster plc",
+            Quantity = 700,
+            Expenses = new() { new() { Description = "Commission", Amount = new(100m) } },
+            GrossProceed = new() { Amount = new(3360m) },
+        };
+
+        Trade trade4 = new()
+        {
+            AssetName = "Lobster plc",
+            BuySell = TradeType.SELL,
+            Date = DateTime.Parse("01-Feb-23 12:33:56"),
+            Description = "Lobster plc",
+            Quantity = 400,
+            Expenses = new() { new() { Description = "Commission", Amount = new(105m) } },
+            GrossProceed = new() { Amount = new(2080m) },
+        };
+        UkSection104Pools section104Pools = new();
+        TaxEventLists taxEventLists = new TaxEventLists();
+        taxEventLists.AddData(new List<TaxEvent>() { trade1, trade2, trade3, trade4 });
+        UkTradeCalculator calculator = new(section104Pools, taxEventLists);
+        List<ITradeTaxCalculation> result = calculator.CalculateTax();
+        result[2].Gain.Amount.ShouldBe(329m, 0.99m);
+        result[3].Gain.Amount.ShouldBe(300m, 0.99m);
     }
 }
