8d481caec89169e35b004b880e08a18a90995cfd
Alex Pun <alexpung@protonmail.com>
2023-11-02 22:31:55 +0000

    Move responsiblity to clear section104 up one level to StartCalculationViewModel

--- BlazorApp-Investment Tax Calculator/ViewModel/StartCalculationViewModel.cs
+++ BlazorApp-Investment Tax Calculator/ViewModel/StartCalculationViewModel.cs
@@ -15,19 +15,22 @@ public partial class StartCalculationViewModel
     private readonly TradeCalculationResult _tradeCalculationResult;
     private readonly DividendCalculationResult _dividendCalculationResult;
     private readonly IMessenger _messenger;
+    private readonly UkSection104Pools _ukSection104Pools;
 
     public StartCalculationViewModel(ITradeCalculator tradeCalculator, TradeCalculationResult tradeCalculationResult,
-        DividendCalculationResult dividendCalculationResult, IDividendCalculator dividendCalculator, IMessenger messenger)
+        DividendCalculationResult dividendCalculationResult, IDividendCalculator dividendCalculator, IMessenger messenger, UkSection104Pools section104Pools)
     {
         _tradeCalculator = tradeCalculator;
         _dividendCalculator = dividendCalculator;
         _tradeCalculationResult = tradeCalculationResult;
         _dividendCalculationResult = dividendCalculationResult;
         _messenger = messenger;
+        _ukSection104Pools = section104Pools;
     }
 
     public async Task OnStartCalculation()
     {
+        _ukSection104Pools.Clear();
         _tradeCalculationResult.SetResult(await Task.Run(_tradeCalculator.CalculateTax));
         _dividendCalculationResult.SetResult(await Task.Run(_dividendCalculator.CalculateTax));
         _messenger.Send<CalculationFinishedMessage>();
