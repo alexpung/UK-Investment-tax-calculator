128357e01fe7606de05d38c0b1e39f63ee28389d
Alex Pun <alexpung@protonmail.com>
2023-11-08 21:45:49 +0000

    Implement spliting trade in future contract trades

--- BlazorApp-Investment Tax Calculator/Model/TaxEvents/FutureContractTrade.cs
+++ BlazorApp-Investment Tax Calculator/Model/TaxEvents/FutureContractTrade.cs
@@ -1,12 +1,38 @@
 ﻿using Enum;
+
 using Model;
 using Model.TaxEvents;
+
+using System.Collections.Immutable;
 
 namespace TaxEvents;
 
 public record FutureContractTrade : Trade
 {
     public required DescribedMoney ContractValue { get; set; }
+    public override AssetCatagoryType AssetType { get; set; } = AssetCatagoryType.FUTURE;
+    public FuturePositionType FuturePositionType { get; set; }
+
+    /// <summary>
+    /// Create a copy of the trade with quantity that is part of the whole trade
+    /// e.g. You buy 5 contract of a future. You can split it by using SplitTrade(2m) and SplitTrade(3m)
+    /// </summary>
+    /// <param name="qty"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public FutureContractTrade SplitTrade(decimal qty)
+    {
+        decimal splitRatio = qty / Quantity;
+        if (qty > Quantity) throw new ArgumentException($"Proportioned trade quantity {qty} must be less than total quantity {Quantity}");
+        return this with
+        {
+            Quantity = qty,
+            Description = $"Part of a trade with quantity {Quantity}",
+            GrossProceed = GrossProceed with { Amount = GrossProceed.Amount * splitRatio },
+            ContractValue = ContractValue with { Amount = ContractValue.Amount * splitRatio },
+            Expenses = Expenses.Select(expense => expense with { Amount = expense.Amount * splitRatio }).ToImmutableList()
+        };
+    }
 
     public override string PrintToTextFile()
     {
