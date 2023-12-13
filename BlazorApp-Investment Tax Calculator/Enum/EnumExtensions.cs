410ae335243ece98a688401790927e8bf37723a1
Alex Pun <alexpung@protonmail.com>
2023-12-13 22:54:18 +0000

    Add a Enum extension method and description for user friendly text

---
+++ BlazorApp-Investment Tax Calculator/Enum/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+namespace Enum2;
+public static class EnumExtensions
+{
+    /// <summary>
+    /// Get human friendly description of the Enum type
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string GetDescription(this System.Enum value)
+    {
+        FieldInfo? field = value.GetType().GetField(value.ToString());
+        if (field == null) return value.ToString();
+        return Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is not DescriptionAttribute attribute
+            ? value.ToString()
+            : attribute.Description;
+    }
+}
--- BlazorApp-Investment Tax Calculator/Enum/FuturePositionType.cs
+++ BlazorApp-Investment Tax Calculator/Enum/FuturePositionType.cs
@@ -1,9 +1,15 @@
-﻿namespace Enum;
+﻿using System.ComponentModel;
+
+namespace Enum;
 
 public enum FuturePositionType
 {
+    [Description("Open long position")]
     OPENLONG,
+    [Description("Open short position")]
     OPENSHORT,
+    [Description("Close long position")]
     CLOSELONG,
+    [Description("Close short position")]
     CLOSESHORT
 }
