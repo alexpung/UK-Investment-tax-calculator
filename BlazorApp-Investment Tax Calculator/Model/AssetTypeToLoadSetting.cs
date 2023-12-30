e602b7ec279539aecf6659720c97e92dbcedf851
Alex Pun <alexpung@protonmail.com>
2023-12-30 11:17:33 +0000

    Enable option to load Fx trades

--- BlazorApp-Investment Tax Calculator/Model/AssetTypeToLoadSetting.cs
+++ BlazorApp-Investment Tax Calculator/Model/AssetTypeToLoadSetting.cs
@@ -4,6 +4,6 @@ public class AssetTypeToLoadSetting
     public bool LoadStocks { get; set; } = true;
     public bool LoadOptions { get; set; }
     public bool LoadFutures { get; set; } = true;
-    public bool LoadFx { get; set; }
+    public bool LoadFx { get; set; } = true;
     public bool LoadDividends { get; set; } = true;
 }
