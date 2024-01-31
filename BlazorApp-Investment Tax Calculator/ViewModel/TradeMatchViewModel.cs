0e0e5d018e441460a6b69e5dd39bbf09bb2e2dbd
Alex Pun <alexpung@protonmail.com>
2024-01-31 23:04:50 +0000

    Add more data to the MatchingDetailTable

--- BlazorApp-Investment Tax Calculator/ViewModel/TradeMatchViewModel.cs
+++ BlazorApp-Investment Tax Calculator/ViewModel/TradeMatchViewModel.cs
@@ -12,9 +12,11 @@ public record TradeMatchViewModel(TradeMatch TradeMatch)
     public string MatchType { get; } = TradeMatch.TradeMatchType.GetDescription();
     public int? AcquistionTradeId { get; } = TradeMatch.MatchedBuyTrade?.Id;
     public int? DisposalTradeId { get; } = TradeMatch.MatchedSellTrade?.Id;
+    public decimal MatchAcquisitionQty { get; } = TradeMatch.MatchAcquisitionQty;
     public decimal MatchDisposalQty { get; } = TradeMatch.MatchDisposalQty;
     public decimal DisposalProceed { get; } = TradeMatch.BaseCurrencyMatchDisposalProceed.Amount;
     public decimal AllowableCost { get; } = TradeMatch.BaseCurrencyMatchAllowableCost.Amount;
     public decimal Gain { get; } = TradeMatch.MatchGain.Amount;
+    public string AdditionalInformation { get; } = TradeMatch.AdditionalInformation;
 
 }
