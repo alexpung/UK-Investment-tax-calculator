77cc450513313e868163ac4c12eff43fef7a7649
Alex Pun <alexpung@protonmail.com>
2024-02-13 23:48:28 +0000

    Make table data persistent.

---
+++ BlazorApp-Investment Tax Calculator/ViewModel/InputGridDatas.cs
@@ -0,0 +1,7 @@
+﻿namespace ViewModel;
+
+public class InputGridDatas
+{
+    public List<TradeInputViewModel> TradeInputViewModels { get; } = [];
+    public List<DividendInputViewModel> DividendInputViewModels { get; } = [];
+}
