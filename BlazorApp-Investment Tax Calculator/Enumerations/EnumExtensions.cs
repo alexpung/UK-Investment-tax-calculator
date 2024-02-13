a497944be9bdafa91c8c3ea52450f30a777bf892
Alex Pun <alexpung@protonmail.com>
2024-02-13 21:30:56 +0000

    Implement Add trade form

--- BlazorApp-Investment Tax Calculator/Enumerations/EnumExtensions.cs
+++ BlazorApp-Investment Tax Calculator/Enumerations/EnumExtensions.cs
@@ -16,4 +16,30 @@ public static class EnumExtensions
             ? value.ToString()
             : attribute.Description;
     }
+
+    public static List<string> GetDescriptions(Type type)
+    {
+        List<string> descriptionList = [];
+        Array enumValues = Enum.GetValues(type);
+        foreach (var enumValue in enumValues)
+        {
+            descriptionList.Add(((Enum)enumValue).GetDescription());
+        }
+        return descriptionList;
+    }
+
+    public static List<EnumDescriptionPair<T>> GetEnumDescriptionPair<T>(Type type) where T : Enum
+    {
+        List<EnumDescriptionPair<T>> enumDescriptionPairList = [];
+        Array enumValues = Enum.GetValues(type);
+        foreach (var enumValue in enumValues)
+        {
+            T castedEnumValue = (T)enumValue;
+            EnumDescriptionPair<T> enumDescriptionPair = new(castedEnumValue, castedEnumValue.GetDescription());
+            enumDescriptionPairList.Add(enumDescriptionPair);
+        }
+        return enumDescriptionPairList;
+    }
 }
+
+public record EnumDescriptionPair<T>(T EnumValue, string Description) where T : Enum;
