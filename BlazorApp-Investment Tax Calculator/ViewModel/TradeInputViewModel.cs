df36992727afe7000b4f4eb01c65a521bfaed9b5
Alex Pun <alexpung@protonmail.com>
2024-02-13 21:28:57 +0000

    Implement trade input view model with validation

---
+++ BlazorApp-Investment Tax Calculator/ViewModel/TradeInputViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Enumerations;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel;
+
+public class TradeInputViewModel
+{
+    private static int _nextId = 0;
+    public int Id { get; init; }
+    [Required]
+    public required string AssetName { get; set; }
+    [Required]
+    public required DateTime Date { get; set; }
+    [Required(ErrorMessage = "Please enter number of unit that are bought or sold. If you are shorting or selling, enter the absolute number here and set Acquisition/Disposal to disposal")]
+    [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater than 0. If you are shorting or selling, enter the absolute number here and set Acquisition/Disposal to disposal")]
+    public required decimal Quantity { get; set; }
+    [Required(ErrorMessage = "Please select if you are buying (acquiring) or selling/shorting (disposing) the asset.")]
+    public required TradeType AcquisitionDisposal { get; set; }
+    [Required(ErrorMessage = "Please select the asset type.")]
+    public required AssetCatagoryType AssetCatagoryType { get; set; }
+    [Required]
+    [CustomValidationCurrencyString]
+    public required string TransactionCurrency { get; set; }
+    [Required]
+    public required decimal GrossProceed { get; set; }
+    [CustomValidationCurrencyString]
+    public string CommissionCurrency { get; set; } = "";
+    public decimal CommissionAmount { get; set; } = 0;
+    [CustomValidationCurrencyString]
+    public string TaxCurrency { get; set; } = "";
+    public decimal TaxAmount { get; set; } = 0;
+    [CustomValidationCurrencyString]
+    public string ContractValueCurrency { get; set; } = "";
+    public decimal ContractValueAmount { get; set; } = 0;
+    public string Description { get; set; } = "";
+
+    public TradeInputViewModel()
+    {
+        Id = Interlocked.Increment(ref _nextId);
+    }
+}
---
+++ BlazorApp-Investment Tax Calculator/ViewModel/ValidationAttributes.cs
@@ -0,0 +1,47 @@
+﻿using Model;
+
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace ViewModel;
+
+/// <summary>
+/// Check if country string is ISO 3166 code. Does not check if entry is blank/null.
+/// </summary>
+public class CustomValidationCompanyLocationStringAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null || string.IsNullOrEmpty((string)value)) return ValidationResult.Success;
+        string inputString = (string)value;
+        try
+        {
+            _ = new RegionInfo(inputString);
+            return ValidationResult.Success;
+        }
+        catch (ArgumentException)
+        {
+            return new ValidationResult("Incorrect ISO 3166 code. Please enter 2 letter ISO 3166 code e.g. US, GB, JP, HK");
+        }
+    }
+}
+/// <summary>
+/// Check if the currency string is ISO 4217 code. Does not check if entry is blank/null.
+/// </summary>
+public class CustomValidationCurrencyStringAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null || string.IsNullOrEmpty((string)value)) return ValidationResult.Success;
+        string inputString = (string)value;
+        try
+        {
+            _ = new WrappedMoney(1, inputString);
+            return ValidationResult.Success;
+        }
+        catch (ArgumentException)
+        {
+            return new ValidationResult("Incorrect ISO 4217 code. Please enter 3 letter ISO 4217 code e.g. USD, JPY, GBP, HKD");
+        }
+    }
+}
