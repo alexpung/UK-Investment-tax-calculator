a45dc8cb089806017f1005766f119b040fc0e810
Alex Pun <alexpung@protonmail.com>
2024-02-15 22:57:38 +0000

    Enhance validation in add trade table

--- BlazorApp-Investment Tax Calculator/ViewModel/TradeInputViewModel.cs
+++ BlazorApp-Investment Tax Calculator/ViewModel/TradeInputViewModel.cs
@@ -18,7 +18,8 @@ public class TradeInputViewModel
     [Required]
     public required DateTime Date { get; set; }
     [Required(ErrorMessage = "Please enter number of unit that are bought or sold. If you are shorting or selling, enter the absolute number here and set Acquisition/Disposal to disposal")]
-    [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater than 0. If you are shorting or selling, enter the absolute number here and set Acquisition/Disposal to disposal")]
+    [Range(0, int.MaxValue, MinimumIsExclusive = true,
+        ErrorMessage = "Quantity must be greater than 0. If you are shorting or selling, enter the absolute number here and set Acquisition/Disposal to disposal")]
     public required decimal Quantity { get; set; }
     [Required(ErrorMessage = "Please select if you are buying (acquiring) or selling/shorting (disposing) the asset.")]
     public required TradeType AcquisitionDisposal { get; set; }
@@ -30,16 +31,16 @@ public class TradeInputViewModel
     [Required(ErrorMessage = "Please enter the amount of local currency you get for selling or you paid for buying. Negative value is allowed and indicate negative price.")]
     public required decimal GrossProceed { get; set; }
     [Required(ErrorMessage = "Please enter the exchange rate used to convert to Sterling")]
-    [Range(0, int.MaxValue, ErrorMessage = "Exchange rate must be greater than 0.")]
-    public required decimal GrossProceedExchangeRate { get; set; }
+    [Range(0, int.MaxValue, MinimumIsExclusive = true, ErrorMessage = "Exchange rate must be greater than 0.")]
+    public required decimal GrossProceedExchangeRate { get; set; } = 1;
     [CustomValidationCurrencyString]
     public string CommissionCurrency { get; set; } = "gbp";
-    [Range(0, int.MaxValue, ErrorMessage = "Exchange rate must be greater than 0.")]
+    [Range(0, int.MaxValue, MinimumIsExclusive = true, ErrorMessage = "Exchange rate must be greater than 0.")]
     public decimal CommissionExchangeRate { get; set; } = 1;
     public decimal CommissionAmount { get; set; } = 0;
     [CustomValidationCurrencyString]
     public string TaxCurrency { get; set; } = "gbp";
-    [Range(0, int.MaxValue, ErrorMessage = "Exchange rate must be greater than 0.")]
+    [Range(0, int.MaxValue, MinimumIsExclusive = true, ErrorMessage = "Exchange rate must be greater than 0.")]
     public decimal TaxExchangeRate { get; set; } = 1;
     public decimal TaxAmount { get; set; } = 0;
     [CustomValidationCurrencyString]
@@ -56,44 +57,60 @@ public class TradeInputViewModel
 
     public Trade Convert()
     {
-        WrappedMoney grossProceed = new(GrossProceed, GrossProceedCurrency);
-        DescribedMoney grossProceedDescribed = new() { Amount = grossProceed, FxRate = GrossProceedExchangeRate };
-        WrappedMoney contractValue = new(ContractValueAmount, ContractValueCurrency);
-        DescribedMoney contractValueDescribed = new() { Amount = contractValue, FxRate = ContractValueExchangeRate };
-        switch (AssetType)
+        DescribedMoney grossProceedDescribed = new(GrossProceed, GrossProceedCurrency, GrossProceedExchangeRate);
+        DescribedMoney contractValueDescribed = new(ContractValueAmount, ContractValueCurrency, ContractValueExchangeRate);
+        List<DescribedMoney> expenses = [];
+        if (TaxAmount != 0) expenses.Add(new(TaxAmount, TaxCurrency, TaxExchangeRate));
+        if (CommissionAmount != 0) expenses.Add(new(CommissionAmount, CommissionCurrency, CommissionExchangeRate));
+        return AssetType switch
         {
-            case AssetCatagoryType.STOCK:
-                return new Trade()
-                {
-                    AssetName = AssetName,
-                    Date = Date,
-                    Quantity = Quantity,
-                    AcquisitionDisposal = AcquisitionDisposal,
-                    AssetType = AssetType,
-                    GrossProceed = grossProceedDescribed
-                };
-            case AssetCatagoryType.FX:
-                return new FxTrade()
-                {
-                    AssetName = AssetName,
-                    Date = Date,
-                    Quantity = Quantity,
-                    AcquisitionDisposal = AcquisitionDisposal,
-                    AssetType = AssetType,
-                    GrossProceed = grossProceedDescribed
-                };
-            case AssetCatagoryType.FUTURE:
-                return new FutureContractTrade()
-                {
-                    AssetName = AssetName,
-                    Date = Date,
-                    Quantity = Quantity,
-                    AcquisitionDisposal = AcquisitionDisposal,
-                    AssetType = AssetType,
-                    GrossProceed = grossProceedDescribed,
-                    ContractValue = contractValueDescribed
-                };
-            default: throw new NotImplementedException();
+            AssetCatagoryType.STOCK => new Trade()
+            {
+                AssetName = AssetName,
+                Date = Date,
+                Quantity = Quantity,
+                AcquisitionDisposal = AcquisitionDisposal,
+                AssetType = AssetType,
+                GrossProceed = grossProceedDescribed,
+                Expenses = [.. expenses],
+                Description = Description
+            },
+            AssetCatagoryType.FX => new FxTrade()
+            {
+                AssetName = AssetName,
+                Date = Date,
+                Quantity = Quantity,
+                AcquisitionDisposal = AcquisitionDisposal,
+                AssetType = AssetType,
+                GrossProceed = grossProceedDescribed,
+                Expenses = [.. expenses],
+                Description = Description
+            },
+            AssetCatagoryType.FUTURE => new FutureContractTrade()
+            {
+                AssetName = AssetName,
+                Date = Date,
+                Quantity = Quantity,
+                AcquisitionDisposal = AcquisitionDisposal,
+                AssetType = AssetType,
+                GrossProceed = new DescribedMoney() { Amount = WrappedMoney.GetBaseCurrencyZero() },
+                ContractValue = contractValueDescribed,
+                Expenses = [.. expenses],
+                Description = Description
+            },
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    public IEnumerable<ValidationResult?> Validate()
+    {
+        if (AssetType == AssetCatagoryType.FUTURE && string.IsNullOrEmpty(ContractValueCurrency))
+        {
+            yield return new ValidationResult("Contract Value Currency is required for a future contract");
+            if (AssetType == AssetCatagoryType.FUTURE && GrossProceed != 0)
+            {
+                yield return new ValidationResult("For a future contract gross proceeds should be set to 0. Profit and loss is calucated from contract values, taxes and commission");
+            }
+            yield return ValidationResult.Success;
         }
     }
-}
