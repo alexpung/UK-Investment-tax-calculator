73a425f3e87a6749c115cb7ac474e6aeaa12083f
Alex Pun <alexpung@protonmail.com>
2024-07-07 13:11:02 +0100

    Add OptionTrade class

---
+++ BlazorApp-Investment Tax Calculator/Model/TaxEvents/OptionTrade.cs
@@ -0,0 +1,17 @@
+﻿using Enumerations;
+
+namespace Model.TaxEvents;
+
+public record OptionTrade : Trade
+{
+    public override AssetCatagoryType AssetType { get; set; } = AssetCatagoryType.OPTION;
+    public required string Underlying { get; set; }
+    public required WrappedMoney StrikePrice { get; set; }
+    public required DateTime ExpiryDate { get; set; }
+
+    public override string PrintToTextFile()
+    {
+        string baseText = base.PrintToTextFile();
+        return $"{baseText}\n\tOption Details: Underlying Asset: {Underlying}, Strike Price: {StrikePrice}, Expiry Date: {ExpiryDate:dd-MMM-yyyy}";
+    }
+}
