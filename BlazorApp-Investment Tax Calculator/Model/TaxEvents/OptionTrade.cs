b1924cad7921dddda0773190ce41e4fbcdf0070b
Alex Pun <alexpung@protonmail.com>
2024-08-10 09:50:36 +0100

    add missing put or call information in OptionTrade class

--- BlazorApp-Investment Tax Calculator/Model/TaxEvents/OptionTrade.cs
+++ BlazorApp-Investment Tax Calculator/Model/TaxEvents/OptionTrade.cs
@@ -8,10 +8,17 @@ public record OptionTrade : Trade
     public required string Underlying { get; set; }
     public required WrappedMoney StrikePrice { get; set; }
     public required DateTime ExpiryDate { get; set; }
+    public required PUTCALL PUTCALL { get; set; }
 
     public override string PrintToTextFile()
     {
         string baseText = base.PrintToTextFile();
         return $"{baseText}\n\tOption Details: Underlying Asset: {Underlying}, Strike Price: {StrikePrice}, Expiry Date: {ExpiryDate:dd-MMM-yyyy}";
     }
+}
+
+public enum PUTCALL
+{
+    PUT,
+    CALL
 }
