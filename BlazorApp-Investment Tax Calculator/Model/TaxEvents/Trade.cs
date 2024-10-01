883affb84d438bef7b93d28c8a3a3283c8639faa
Alex Pun <alexpung@protonmail.com>
2024-10-01 23:28:37 +0100

    Supress warning

--- BlazorApp-Investment Tax Calculator/Model/TaxEvents/Trade.cs
+++ BlazorApp-Investment Tax Calculator/Model/TaxEvents/Trade.cs
@@ -26,7 +26,9 @@ public record Trade : TaxEvent, ITextFilePrintable
             _quantity = value;
         }
     }
+#pragma warning disable CS8618 // backfield already set in required property GrossProceed
     private DescribedMoney _grossProceed;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     /// <summary>
     /// Greater than 0 regardless of acquisition or disposal
     /// </summary>
