97e3703e4cc307c79add9a5508a718b6c5e60bf1
Alex Pun <alexpung@protonmail.com>
2024-11-01 13:58:35 +0000

    Improve trade calculation export

--- BlazorApp-Investment Tax Calculator/Services/TaxYearCgtByTypeReportService.cs
+++ BlazorApp-Investment Tax Calculator/Services/TaxYearCgtByTypeReportService.cs
@@ -32,4 +32,22 @@ public class TaxYearCgtByTypeReportService(TradeCalculationResult tradeCalculati
             };
         }
     }
+
+    public TaxYearCgtByTypeReport GetTaxYearCgtByTypeReports(int taxYear)
+    {
+        return new TaxYearCgtByTypeReport()
+        {
+            TaxYear = taxYear,
+            ListedSecurityNumberOfDisposals = tradeCalculationResult.NumberOfDisposals([taxYear], AssetGroupType.LISTEDSHARES),
+            ListedSecurityDisposalProceeds = tradeCalculationResult.DisposalProceeds([taxYear], AssetGroupType.LISTEDSHARES).Amount,
+            ListedSecurityAllowableCosts = tradeCalculationResult.AllowableCosts([taxYear], AssetGroupType.LISTEDSHARES).Amount,
+            ListedSecurityGainExcludeLoss = tradeCalculationResult.TotalGain([taxYear], AssetGroupType.LISTEDSHARES).Amount,
+            ListedSecurityLoss = tradeCalculationResult.TotalLoss([taxYear], AssetGroupType.LISTEDSHARES).Amount,
+            OtherAssetsNumberOfDisposals = tradeCalculationResult.NumberOfDisposals([taxYear], AssetGroupType.OTHERASSETS),
+            OtherAssetsDisposalProceeds = tradeCalculationResult.DisposalProceeds([taxYear], AssetGroupType.OTHERASSETS).Amount,
+            OtherAssetsAllowableCosts = tradeCalculationResult.AllowableCosts([taxYear], AssetGroupType.OTHERASSETS).Amount,
+            OtherAssetsGainExcludeLoss = tradeCalculationResult.TotalGain([taxYear], AssetGroupType.OTHERASSETS).Amount,
+            OtherAssetsLoss = tradeCalculationResult.TotalLoss([taxYear], AssetGroupType.OTHERASSETS).Amount
+        };
+    }
 }
--- BlazorApp-Investment Tax Calculator/Services/UkCalculationResultExportService.cs
+++ BlazorApp-Investment Tax Calculator/Services/UkCalculationResultExportService.cs
@@ -6,14 +6,17 @@ using System.Text;
 
 namespace InvestmentTaxCalculator.Services;
 
-public class UkCalculationResultExportService(ITaxYear taxYear, TradeCalculationResult tradeCalculationResult) : ITextFilePrintable
+public class UkCalculationResultExportService(ITaxYear taxYear, TradeCalculationResult tradeCalculationResult,
+    TaxYearCgtByTypeReportService taxYearCgtByTypeReportService, TaxYearReportService taxYearReportService) : ITextFilePrintable
 {
+    private IEnumerable<TaxYearCgtReport>? _taxYearReports;
     public string PrintToTextFile(IEnumerable<int> yearsToExport)
     {
+        _taxYearReports = taxYearReportService.GetTaxYearReports();
         StringBuilder output = new();
         foreach (int year in yearsToExport.OrderByDescending(i => i))
         {
-            output.Append(WriteTaxYearSummary(year, tradeCalculationResult));
+            output.Append(WriteTaxYearSummary(year));
             IEnumerable<ITradeTaxCalculation> yearFilteredTradeCalculations = tradeCalculationResult
                 .CalculatedTrade.Where(i => taxYear.ToTaxYear(i.Date) == year && i.AcquisitionDisposal == TradeType.DISPOSAL)
                 .OrderBy(i => i.Date);
@@ -31,28 +34,61 @@ public class UkCalculationResultExportService(ITaxYear taxYear, TradeCalculation
         return PrintToTextFile(taxYears);
     }
 
-    private static string WriteTaxYearSummary(int year, TradeCalculationResult calculationResult)
+    private string WriteTaxYearSummary(int year)
     {
+        TaxYearCgtReport? taxYearCgtReport = _taxYearReports?.FirstOrDefault(report => report.TaxYear == year);
+        TaxYearCgtByTypeReport taxYearCgtByTypeReport = taxYearCgtByTypeReportService.GetTaxYearCgtByTypeReports(year);
         StringBuilder output = new();
-        IEnumerable<int> filter = [year];
         output.AppendLine($"Summary for tax year {year}:");
-        output.AppendLine($"Number of disposals: {calculationResult.NumberOfDisposals(filter)}");
-        output.AppendLine($"Total disposal proceeds: {calculationResult.DisposalProceeds(filter)}");
-        output.AppendLine($"Total allowable costs: {calculationResult.AllowableCosts(filter)}");
-        output.AppendLine($"Total gains (excluding loss): {calculationResult.TotalGain(filter)}");
-        output.AppendLine($"Total loss: {calculationResult.TotalLoss(filter)}");
+        if (taxYearCgtReport != null) WriteTaxYearOverallSummary(output, taxYearCgtReport);
+        WriteTaxYearByTypeSummary(output, taxYearCgtByTypeReport);
         return output.ToString();
+    }
+
+    private static void WriteTaxYearOverallSummary(StringBuilder output, TaxYearCgtReport taxYearCgtReport)
+    {
+        output.AppendLine($"Total gain in year {taxYearCgtReport.TotalGainInYear}");
+        output.AppendLine($"Total loss in year {taxYearCgtReport.TotalLossInYear}");
+        output.AppendLine($"Net gain in year {taxYearCgtReport.NetCapitalGain}");
+        output.AppendLine($"Capital allowance available {taxYearCgtReport.CapitalGainAllowance}");
+        output.AppendLine($"Capital loss brought forward and used {taxYearCgtReport.CgtAllowanceBroughtForwardAndUsed}");
+        output.AppendLine($"Taxable gain after allowance and offset {taxYearCgtReport.TaxableGainAfterAllowanceAndLossOffset}");
+        output.AppendLine($"Loss available to bring forward {taxYearCgtReport.LossesAvailableToBroughtForward}");
+    }
+
+    private static void WriteTaxYearByTypeSummary(StringBuilder output, TaxYearCgtByTypeReport taxYearCgtByTypeReport)
+    {
+        output.AppendLine();
+        output.AppendLine("Listed Shares and Securities:");
+        output.AppendLine($"Number of Disposals {taxYearCgtByTypeReport.ListedSecurityNumberOfDisposals}");
+        output.AppendLine($"Disposal Proceeds {taxYearCgtByTypeReport.ListedSecurityDisposalProceeds}");
+        output.AppendLine($"Allowable costs {taxYearCgtByTypeReport.ListedSecurityAllowableCosts}");
+        output.AppendLine($"Gain excluding loss {taxYearCgtByTypeReport.ListedSecurityGainExcludeLoss}");
+        output.AppendLine($"Loss {taxYearCgtByTypeReport.ListedSecurityLoss}");
+        output.AppendLine();
+        output.AppendLine("Other assets:");
+        output.AppendLine($"Number of Disposals {taxYearCgtByTypeReport.OtherAssetsNumberOfDisposals}");
+        output.AppendLine($"Disposal Proceeds {taxYearCgtByTypeReport.OtherAssetsDisposalProceeds}");
+        output.AppendLine($"Allowable costs {taxYearCgtByTypeReport.OtherAssetsAllowableCosts}");
+        output.AppendLine($"Gain excluding loss {taxYearCgtByTypeReport.OtherAssetsGainExcludeLoss}");
+        output.AppendLine($"Loss {taxYearCgtByTypeReport.OtherAssetsLoss}");
     }
 
     private static string WriteDisposalDetails(IEnumerable<ITradeTaxCalculation> tradeTaxCalculations)
     {
         StringBuilder output = new();
-        int DisposalCount = 1;
-        foreach (var calculations in tradeTaxCalculations)
+        var GroupedTradeTaxCalculations = tradeTaxCalculations.GroupBy(tradeTaxCalculations => tradeTaxCalculations.AssetCategoryType);
+        foreach (var Grouping in GroupedTradeTaxCalculations)
         {
             output.AppendLine("*******************************************************************************");
-            output.Append($"Disposal {DisposalCount}: {calculations.PrintToTextFile()}");
-            DisposalCount++;
+            output.AppendLine($"Disposal for {Grouping.Key.GetDescription()}");
+            int DisposalCount = 1;
+            foreach (var calculations in Grouping)
+            {
+                output.AppendLine("*******************************************************************************");
+                output.Append($"Disposal {DisposalCount}: {calculations.PrintToTextFile()}");
+                DisposalCount++;
+            }
         }
         return output.ToString();
     }
