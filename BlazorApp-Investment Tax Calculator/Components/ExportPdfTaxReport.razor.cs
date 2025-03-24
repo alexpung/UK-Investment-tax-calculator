5626f5652f8262ca118ecb49b66a765af20ec0b9
Alex Pun <alexpung@protonmail.com>
2025-03-24 20:23:54 +0000

    Introduce a listbox to allow ordering and enable/disable of sections in export PDF

--- BlazorApp-Investment Tax Calculator/Components/ExportPdfTaxReport.razor.cs
+++ BlazorApp-Investment Tax Calculator/Components/ExportPdfTaxReport.razor.cs
@@ -4,6 +4,8 @@ using InvestmentTaxCalculator.Services.PdfExport;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
+using Syncfusion.Blazor.DropDowns;
+
 namespace InvestmentTaxCalculator.Components;
 public partial class ExportPdfTaxReport
 {
@@ -12,8 +14,9 @@ public partial class ExportPdfTaxReport
     [Inject] public required YearOptions YearsToExport { get; set; }
     [Inject] public required ToastService ToastService { get; set; }
 
-    private IJSObjectReference? _downloadFileJsScript;
+    public SfListBox<string[], ISection> SectionsSelection { get; set; } = new();
 
+    private IJSObjectReference? _downloadFileJsScript;
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         _downloadFileJsScript = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Components/ExportFile.razor.js");
@@ -28,10 +31,37 @@ public partial class ExportPdfTaxReport
                 using var streamRef = new DotNetStreamReference(PdfExportService.CreatePdf(year));
                 await _downloadFileJsScript!.InvokeVoidAsync("BlazorDownloadFile", $"Tax Report {year}.pdf", streamRef);
             }
+            catch (InvalidOperationException ex)
+            {
+                ToastService.ShowWarning(ex.Message);
+            }
             catch (Exception ex)
             {
                 ToastService.ShowError(ex.Message);
+                if (!string.IsNullOrEmpty(ex.StackTrace))
+                {
+                    ToastService.ShowError(ex.StackTrace);
+                }
             }
+        }
+    }
+
+    private static void OnDrop(DropEventArgs<ISection> args)
+    {
+        if (args.Items.Count() > 1)
+        {
+            args.Items = [args.Items.First()];
+        }
+    }
+    private void OnDropped(DropEventArgs<ISection> args)
+    {
+        if (!args.Items.Any()) return;
+        ISection movedSection = args.Items.First();
+        int oldIndex = PdfExportService.AllSections.IndexOf(movedSection);
+        if (oldIndex != -1)
+        {
+            PdfExportService.AllSections.RemoveAt(oldIndex);
+            PdfExportService.AllSections.Insert(args.DropIndex, movedSection);
         }
     }
 }
