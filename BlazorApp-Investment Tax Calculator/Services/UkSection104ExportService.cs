b7b5f693b63f82653006fed7b72b8ad18efe6695
Alex Pun <alexpung@protonmail.com>
2025-12-17 23:28:08 +0000

    Implement limit on date range on S104 text and PDF export.

--- BlazorApp-Investment Tax Calculator/Services/UkSection104ExportService.cs
+++ BlazorApp-Investment Tax Calculator/Services/UkSection104ExportService.cs
@@ -5,17 +5,22 @@ using System.Text;
 
 namespace InvestmentTaxCalculator.Services;
 
-public class UkSection104ExportService(ITaxYear taxYear, UkSection104Pools ukSection104Pools) : ITextFilePrintable
+public class UkSection104ExportService(UkSection104Pools ukSection104Pools) : ITextFilePrintable
 {
     public string PrintToTextFile(IEnumerable<int> yearsToExport)
     {
         StringBuilder output = new();
-        output.AppendLine("Section 104 detail history:");
-        foreach (var pool in ukSection104Pools.GetActiveSection104s(yearsToExport))
+        if (!yearsToExport.Any())
         {
-            output.AppendLine($"Asset Name {pool.AssetName}");
+            return string.Empty;
+        }
+        int endReportYear = yearsToExport.Max();
+        output.AppendLine("Section 104 detail history:");
+        foreach (string assetName in ukSection104Pools.GetActiveSection104s(yearsToExport).Select(pool => pool.AssetName))
+        {
+            output.AppendLine($"Asset Name {assetName}");
             output.AppendLine("Date\t\tNew Quantity (change)\t\tNew Value (change)\t\tContract value (for futures)");
-            foreach (var history in pool.Section104HistoryList)
+            foreach (var history in ukSection104Pools.GetSection104HistoriesUnitlTaxYear(endReportYear, assetName))
             {
                 output.AppendLine(history.PrintToTextFile());
             }
