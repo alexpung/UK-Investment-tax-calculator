059b34c6c03192559d991a98fbc7e8d06d7b3dd0
Alex Pun <alexpung@protonmail.com>
2026-02-10 21:06:00 +0000

    fix tests

--- UnitTest/Test/TradeCalculations/GroupedTradeContainerTest.cs
+++ UnitTest/Test/TradeCalculations/GroupedTradeContainerTest.cs
@@ -109,36 +109,6 @@ public class GroupedTradeContainerTests
         xyzEvents.Events[2].Date.ShouldBe(DateTime.Parse("05-Jan-23 10:00:00", CultureInfo.InvariantCulture)); // Second trade
     }
 
-    [Fact]
-    public void GetAllTaxEventsGroupedAndSorted_WithNullTickerOrderOnCorporateAction_ShouldFallbackToAssetName()
-    {
-        // Arrange
-        var tradeList = new List<ITradeTaxCalculation>
-        {
-            CreateTrade("ABC", DateTime.Parse("01-Jan-23 10:00:00", CultureInfo.InvariantCulture)),
-            CreateTrade("ABC", DateTime.Parse("03-Jan-23 10:00:00", CultureInfo.InvariantCulture))
-        };
-
-        var corporateAction = Substitute.For<CorporateAction>();
-        corporateAction.AssetName.Returns("ABC");
-        corporateAction.Date.Returns(DateTime.Parse("02-Jan-23 10:00:00", CultureInfo.InvariantCulture));
-        corporateAction.CompanyTickersInProcessingOrder.Returns((IReadOnlyList<string>?)null);
-
-        var container = new GroupedTradeContainer<ITradeTaxCalculation>(tradeList, [corporateAction]);
-
-        // Act
-        var allEvents = container.GetAllTaxEventsGroupedAndSorted().ToList();
-
-        // Assert
-        allEvents.Count.ShouldBe(1);
-
-        var abcEvents = allEvents.Single(e => e.AssetName == "ABC");
-        abcEvents.Events.Count.ShouldBe(3);
-        abcEvents.Events[0].Date.ShouldBe(DateTime.Parse("01-Jan-23 10:00:00", CultureInfo.InvariantCulture));
-        abcEvents.Events[1].Date.ShouldBe(DateTime.Parse("02-Jan-23 10:00:00", CultureInfo.InvariantCulture));
-        abcEvents.Events[2].Date.ShouldBe(DateTime.Parse("03-Jan-23 10:00:00", CultureInfo.InvariantCulture));
-    }
-
     // Helper methods to create mocked trades and corporate actions
     private static ITradeTaxCalculation CreateTrade(string assetName, DateTime date)
     {
@@ -152,6 +122,7 @@ public class GroupedTradeContainerTests
     {
         var corporateAction = Substitute.For<CorporateAction>();
         corporateAction.AssetName.Returns(assetName);
+        corporateAction.CompanyTickersInProcessingOrder.Returns([assetName]);
         corporateAction.Date.Returns(date);
         return corporateAction;
     }
